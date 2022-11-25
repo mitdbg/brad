83cce0496c47d15b0f001fb197a50c005c152e9b
Geoffrey Yu <geoffxy@gmail.com>
2022-11-24 20:27:57 -0500

    Run experiments 3 times, rename combine tasks

--- cirrus/experiments/04-postgres3/run_postgres.sh
+++ cirrus/experiments/04-postgres3/run_postgres.sh
@@ -4,8 +4,11 @@ script_loc=$(cd $(dirname $0) && pwd -P)
 cd $script_loc
 source ../experiment_config.sh
 
-../../build/store_sale \
-  --pg_odbc_dsn="$PG_ODBC_DSN" \
-  --pg_user=$PG_ODBC_USER \
-  --pwdvar=$PWDVAR \
-  $@ > $COND_OUT/results.csv
+# Repeat experiment 3 times
+for i in $(seq 1 3); do
+  ../../build/store_sale \
+    --pg_odbc_dsn="$PG_ODBC_DSN" \
+    --pg_user=$PG_ODBC_USER \
+    --pwdvar=$PWDVAR \
+    $@ > $COND_OUT/results-$i.csv
+done
