3a3c61575069375b6a7f0e1142129f8e6b50abc9
Markos Markakis <markakis@mit.edu>
2023-02-23 10:33:57 -0500

    QueryParser finds clauses

--- src/iohtap/forecasting/forecaster.py
+++ src/iohtap/forecasting/forecaster.py
@@ -1,4 +1,12 @@
+from iohtap.forecasting.query_parser import QueryParser
+
+
 class WorkloadForecaster:
     def forecast(self):
         print("I am a placeholder")
         return
+
+    def process(self, sql_query):
+        p = QueryParser()
+        l = p.get_clauses(sql_query)
+        return l
---
+++ src/iohtap/forecasting/query_parser.py
@@ -0,0 +1,24 @@
+import re
+
+CLAUSE_KEYWORDS = ["SELECT", "FROM", "WHERE", "GROUP BY", "HAVING", "ORDER BY"]
+
+
+class QueryParser:
+    def __init__(self):
+        self._keywords = [i.lower() for i in CLAUSE_KEYWORDS]
+        self._keywords.extend(CLAUSE_KEYWORDS)
+
+    def get_clauses(self, sql_query):
+        l = re.split(f"({'|'.join(self._keywords)})", sql_query)
+        d = {}
+        for i, item in enumerate(l):
+            if item in self._keywords:
+                d[item.upper()] = l[i + 1].strip()
+
+        return d
+
+
+if __name__ == "__main__":
+    s = "SELECT * FROM A where A.x=2"
+    p = QueryParser()
+    print(p.get_clauses(s))
