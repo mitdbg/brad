18bd37cd128c09024ba22ae8c3f81d1ac2a73c63
Markos Markakis <markakis@mit.edu>
2023-02-23 13:29:17 -0500

    Parse attributes, create num join histogram

--- src/iohtap/forecasting/forecaster.py
+++ src/iohtap/forecasting/forecaster.py
@@ -4,20 +4,41 @@ from iohtap.forecasting.query_parser import QueryParser
 class WorkloadForecaster:
     def __init__(self):
         self._parser = QueryParser()
+        self._num_joins_histogram = [0 for _ in range(11)]
+        self._total_queries = 0
+
+    def get_template_frequency(self, num_joins):
+        return self._num_joins_histogram[num_joins] / self._total_queries
 
     def forecast(self):
         print("I am a placeholder")
         return
 
     def process(self, sql_query):
-        clause_dict = self._parser.get_clauses(sql_query)
+        clause_dict = self._parser.get_clauses(sql_query.rstrip(";"))
 
-        num_joins = self._parser.get_num_joins(clause_dict)
+        (
+            join_predicates,
+            filtered_attributes,
+        ) = self._parser.get_predicates_and_filtered_attributes(clause_dict)
 
-        return num_joins
+        self._num_joins_histogram[len(join_predicates)] += 1
+        self._total_queries += 1
+
+        return (
+            join_predicates,
+            len(join_predicates),
+            filtered_attributes,
+            len(filtered_attributes),
+        )
 
 
 if __name__ == "__main__":
-    s = "WHERE cn.country_code ='[us]' AND k.keyword ='character-name-in-.title' AND n.name LIKE 'B%' AND n.id = ci.person_id AND ci.movie_id = t.id AND t.id = mk.movie_id AND mk.keyword_id = k.id AND t.id = mc.movie_id AND mc.company_id = cn.id AND ci.movie_id = mc.movie_id AND ci.movie_id = mk.movie_id AND mc.movie_id = mk.movie_id;"
+    q1 = "SELECT MIN(chn.name) AS uncredited_voiced_character, MIN(t.title) AS russian_movie FROM char_name AS chn, cast_info AS ci, company_name AS cn, company_type AS ct, movie_companies AS mc, role_type AS rt, title AS  t WHERE ci.note LIKE '%(voice)%' AND ci.note LIKE '%(uncredited)%' AND cn.country_code = '[ru]' AND rt.role = 'actor' AND t.production_year > 2005 AND t.id = mc.movie_id AND t.id = ci.movie_id AND ci.movie_id = mc.movie_id AND chn.id = ci.person_role_id AND rt.id = ci.role_id AND cn.id = mc.company_id AND ct.id = mc.company_type_id;"
+    q2 = "SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate FROM Orders INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;"
     f = WorkloadForecaster()
-    print(f.process(s))
+    print(f.process(q1))
+    print(f.process(q2))
+
+    for i in range(11):
+        print(f.get_template_frequency(i))
--- src/iohtap/forecasting/query_parser.py
+++ src/iohtap/forecasting/query_parser.py
@@ -19,23 +19,29 @@ class QueryParser:
 
         return d
 
-    def get_num_joins(self, d):
-        # Count number of joins in the query. Joins may either be in the FROM clause
+    def get_predicates_and_filtered_attributes(self, d):
+        # Find join predicates in the query. Joins may either be in the FROM clause
         # or in the WHERE clause.
-        num_joins = 0
+        predicates = []
+
+        # Also find the attributes that are targets of filters in the queries
+        filtered_attributes = []
 
         if "FROM" in d.keys():
-            num_joins += d["FROM"].count("JOIN")
+            tables = d["FROM"].split(",")
+            predicates.extend(
+                [t.split("ON")[1].strip() for t in tables if len(t.split("ON")) >= 2]
+            )
         if "WHERE" in d.keys():
             # Match the pattern period - equals sign - period, with the periods not inside single quotes.
-            num_joins += sum(
-                [
-                    int(bool(re.match(r"[^']*\.[^']*=[^']*\.[^']*", i)))
-                    for i in d["WHERE"].split("AND")
-                ]
-            )
 
-        return num_joins
+            for i in d["WHERE"].split("AND"):
+                if bool(re.match(r"[^']*\.[^']*=[^']*\.[^']*", i)):
+                    predicates.append(i.strip())
+                else:
+                    filtered_attributes.append(i.split()[0].strip())
+
+        return predicates, filtered_attributes
 
 
 if __name__ == "__main__":
