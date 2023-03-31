c92d7c62ad12a5c6bc3d2be301b6f29587d7477e
Markos Markakis <mmarkakis98@gmail.com>
2023-03-31 11:40:47 -0400

    Better format

--- src/brad/forecasting/metric_forecaster.py
+++ src/brad/forecasting/metric_forecaster.py
@@ -1,10 +1,12 @@
 from brad.forecasting.metric_reader import MetricReader
 from typing import List, Dict
+from datetime import timedelta
 
 
 class MetricForecaster:
     def __init__(
         self,
+        redshift_cluster_id: str,
         redshift_metrics: Dict[str, List[str]],
         aurora_metrics: Dict[str, List[str]],
         epoch_minutes=60 * 24,
@@ -15,49 +17,60 @@ class MetricForecaster:
 
         self.readers = {}
         for metric in self.redshift_metrics.keys():
-            for stat in self.redshift_metrics[metric]:
-                name = "redshift_" + metric + "_" + stat
-                self.readers[name] = MetricReader(
-                    "redshift", metric, stat, epoch_minutes
-                )
+            name = "redshift_" + metric
+            self.readers[name] = MetricReader(
+                "redshift",
+                metric,
+                self.redshift_metrics[metric],
+                epoch_minutes,
+                redshift_cluster_id,
+            )
 
         for metric in self.aurora_metrics.keys():
-            for stat in self.aurora_metrics[metric]:
-                name = "aurora_" + metric + "_" + stat
-                self.readers[name] = MetricReader("aurora", metric, stat, epoch_minutes)
+            name = "aurora_" + metric
+            self.readers[name] = MetricReader(
+                "aurora", metric, self.aurora_metrics[metric], epoch_minutes
+            )
 
     # Stats for the i-th epoch. Epoch 0 is currently in progress, positive epochs are in the future.
     # Returns actual stats for negative values of i, forecasts for other values of i.
     def at_epoch(self, i: int):
-        d = {}
+        l = []
         for reader in self.readers.keys():
-            value = (
-                self.readers[reader].get_stats(i) if i < 0 else self.forecast(reader, i)
+            l.extend(
+                self.readers[reader].get_stats(i, i + 1)
+                if i < 0
+                else self.forecast_impl(reader, i)
             )
-            d[reader] = value
 
-        return d
+        return l
 
-    # Values for a specific metric-stat pair from start_epoch (inclusive) until end_epoch (exclusive)
-    def by_metric(self, metric_stat: str, start_epoch: int, end_epoch: int):
+    # Values for a specific metric from start_epoch (inclusive) until end_epoch (exclusive)
+    def by_metric(self, service: str, metric: str, start_epoch: int, end_epoch: int):
         if start_epoch > end_epoch:
             raise ValueError("start_epoch must be less than or equal to end_epoch")
 
+        metric_name = f"{service}_{metric}"
         l = []
-        i = start_epoch
-        for i in range(start_epoch, end_epoch):
-            l.append(
-                self.readers[metric_stat].get_stats(i)
-                if i < 0
-                else self.forecast(metric_stat, i)
+        if start_epoch < 0:
+            l.extend(
+                self.readers[metric_name].get_stats(start_epoch, min(end_epoch, 0))
             )
+        for i in range(max(start_epoch, 0), max(end_epoch, 0)):
+            l.extend(self.forecast(service, metric, i))
         return l
 
-    # Forecast the value of a specific metric-stat pair at epoch i.
-    def forecast(self, metric_stat: str, i: int):
+    # Forecast the value of a specific metric at epoch i.
+    def forecast(self, service: str, metric: str, i: int):
+        return self.forecast_impl(f"{service}_{metric}", i)
+
+    def forecast_impl(self, metric_name: str, i: int):
         if i < 0:
             raise ValueError("Can only forecast values for future epochs.")
-        return self.readers[metric_stat].get_stats(-1)
+        most_recent = self.readers[metric_name].get_stats(-1, 0)
+        most_recent[0]["EpochStart"] += timedelta(minutes=self.epoch_minutes) * (i + 1)
+        most_recent[0]["Epoch"] = i
+        return most_recent
 
 
 if __name__ == "__main__":
@@ -67,12 +80,12 @@ if __name__ == "__main__":
     }
     am = {"CPUUtilization": ["Average", "Maximum"]}
 
-    fc = MetricForecaster(rm, am)
+    fc = MetricForecaster("brad-redshift", rm, am)
     print(fc.at_epoch(-2))
     print(fc.at_epoch(-1))
     print(fc.at_epoch(0))
     print(fc.at_epoch(1))
 
-    print(fc.by_metric("redshift_CPUUtilization_Average", -3, 2))
-    print(fc.by_metric("redshift_CPUUtilization_Average", -2, 0))
-    print(fc.by_metric("redshift_CPUUtilization_Average", 1, 3))
+    print(fc.by_metric("redshift", "CPUUtilization", -3, 2))
+    print(fc.by_metric("redshift", "CPUUtilization", -2, 0))
+    print(fc.by_metric("redshift", "CPUUtilization", 1, 3))
