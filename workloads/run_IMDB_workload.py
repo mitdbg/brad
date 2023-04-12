5e89a5783e69e37a664909cd958eb353094dcae6
wuziniu <wuziniu2015@gmail.com>
2023-04-12 15:03:31 -0400

    workload runner

--- workloads/run_IMDB_workload.py
+++ workloads/run_IMDB_workload.py
@@ -1,6 +1,6 @@
 import asyncio
 from datetime import timedelta
-from typing import AsyncIterator, final
+from typing import AsyncIterator, final, Generator, Optional
 
 from typing_extensions import override
 
@@ -12,39 +12,32 @@ from workloads.runner.schedule import Once, Repeat
 from workloads.runner.time import get_current_time
 from workloads.runner.user import User
 from workloads.runner.workload import Workload
+from brad.grpc_client import BradGrpcClient
+from brad.config.session import SessionId
 
-
-@final
-class NoopClient(AsyncClient[str]):
-    @override
-    async def connect(self) -> None:
-        return
-
-    @override
-    async def close(self) -> None:
-        return
-
-    @override
-    async def execute(self, query: str) -> AsyncIterator[str]:
-        yield query
-
-
-@final
 class BradClient(AsyncClient[str]):
+    def __init__(self, host: str, port: int):
+        self._impl = BradGrpcClient(host, port)
+        self._session_id: Optional[SessionId] = None
     @override
     async def connect(self) -> None:
-        return
+        self._impl.connect()
+        self._session_id = self._impl._session_id
 
     @override
     async def close(self) -> None:
-        return
+        self._impl.close()
+        self._session_id = None
 
     @override
     async def execute(self, query: str) -> AsyncIterator[str]:
-        yield query
+        res = self._impl.run_query(query)
+        for tup in res:
+            yield tup
 
 
-async def main() -> None:
+async def run_IMDB() -> None:
+
     current_time = get_current_time()
     interval = timedelta(seconds=1)
 
@@ -52,7 +45,7 @@ async def main() -> None:
         [
             Workload.serial(
                 [
-                    Query(f"SELECT {i};", Once(at=current_time + 0.47 * i * interval))
+                    Query(f"SELECT COUNT(*) FROM info_type WHERE id > {i};", Once(at=current_time + 0.47 * i * interval))
                     for i in range(10)
                 ],
                 user=User.with_label("Once"),
@@ -60,7 +53,7 @@ async def main() -> None:
             Workload.serial(
                 [
                     Query(
-                        "SELECT *;",
+                        "SELECT COUNT(*) FROM title;",
                         Repeat.starting_now(interval=interval, num_repeat=20),
                     ),
                 ],
@@ -71,8 +64,9 @@ async def main() -> None:
 
     reporter = PrintReporter()
 
-    async with NoopClient() as client:
+    async with BradClient(host="0.0.0.0", port=6583) as client:
         await run_workload(workload, client, reporter)
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(run_IMDB())
