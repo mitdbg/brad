9945fd954d839a083d962987cb91dc1590ea3cce
Geoffrey Yu <geoffxy@mit.edu>
2023-04-17 14:24:23 -0400

    Add known filters with sanity check tests

--- src/brad/planner/filters/aurora_transactions.py
+++ src/brad/planner/filters/aurora_transactions.py
@@ -1,3 +1,6 @@
+from brad.blueprint import Blueprint
+from brad.config.engine import Engine
+from brad.planner.workload import Workload
 from .filter import Filter
 
 
@@ -5,3 +8,19 @@ class AuroraTransactions(Filter):
     """
     Ensures that any tables accessed transactionally are present on Aurora.
     """
+
+    def __init__(self, workload: Workload) -> None:
+        self._transactional_tables = set()
+        for template in workload.templates():
+            if not template.is_transactional():
+                continue
+            self._transactional_tables.update(template.tables())
+
+    def is_valid(self, candidate: Blueprint) -> bool:
+        for table in candidate.tables():
+            if (
+                table.name in self._transactional_tables
+                and Engine.Aurora not in table.locations
+            ):
+                return False
+        return True
--- src/brad/planner/filters/table_on_engine.py
+++ src/brad/planner/filters/table_on_engine.py
@@ -1,3 +1,6 @@
+from brad.blueprint import Blueprint
+from brad.config.engine import Engine
+
 from .filter import Filter
 
 
@@ -6,3 +9,15 @@ class TableOnEngine(Filter):
     Ensures that a table is only placed on an engine if it has at least one node
     (or is Athena).
     """
+
+    def is_valid(self, candidate: Blueprint) -> bool:
+        has_aurora = candidate.aurora_provisioning().num_nodes() > 0
+        has_redshift = candidate.redshift_provisioning().num_nodes() > 0
+
+        for table in candidate.tables():
+            if not has_aurora and Engine.Aurora in table.locations:
+                return False
+            if not has_redshift and Engine.Redshift in table.locations:
+                return False
+
+        return True
