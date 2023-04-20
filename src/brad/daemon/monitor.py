b39e9c81305aec2e08cc9e0b04f761ceb69a2baf
Markos Markakis <mmarkakis98@gmail.com>
2023-04-20 11:46:04 -0400

    one epoch across engines

--- src/brad/daemon/monitor.py
+++ src/brad/daemon/monitor.py
@@ -1,28 +1,38 @@
 from brad.config.file import ConfigFile
 import importlib.resources as pkg_resources
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import json
 from brad.config.engine import Engine
 import brad.daemon as daemon
+from datetime import timedelta
 
 
 class Monitor:
     def __init__(self, config: ConfigFile) -> None:
         self._config = config
-        self._metrics = self._load_monitored_metrics()
+        self._epoch_length, self._metrics = self._load_monitored_metrics()
 
     async def run_forever(self) -> None:
         # Flesh out the monitor - maintain running averages of the underlying
         # engines' metrics.
         pass
 
-    def _load_monitored_metrics(self) -> Dict[str, Dict[str, List[str]]]:
+    def _load_monitored_metrics(
+        self,
+    ) -> Tuple[Dict[str, Dict[str, List[str]]], timedelta]:
         # Load data.
         with pkg_resources.open_text(daemon, "monitored_metrics.json") as data:
             file_contents = json.load(data)
-        metrics_map = {}
 
-        for f in file_contents:
+        epoch_length = timedelta(
+            weeks=file_contents["epoch_length"]["weeks"],
+            days=file_contents["epoch_length"]["days"],
+            hours=file_contents["epoch_length"]["hours"],
+            minutes=file_contents["epoch_length"]["minutes"],
+        )
+
+        metrics_map = {}
+        for f in file_contents["monitored_metrics"]:
             try:
                 eng_name = Engine.from_str(f["engine"])
             except:
@@ -33,7 +43,7 @@ class Monitor:
             for m in f["metrics"]:
                 metrics_map[eng_name][m] = f["metrics"][m]
 
-        return metrics_map
+        return epoch_length, metrics_map
 
 
 if __name__ == "__main__":
