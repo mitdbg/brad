e52f586b225e03bdf6b2e90619b7a73a1d28a4c1
Geoffrey Yu <geoffxy@mit.edu>
2023-04-27 19:41:05 -0400

    Add utility to extract table sizes

--- src/brad/planner/workload/workload.py
+++ src/brad/planner/workload/workload.py
@@ -1,16 +1,53 @@
-from typing import List
+from typing import Dict, List, Tuple, Optional
 
 from .query_template import QueryTemplate
+from brad.blueprint import Blueprint
+from brad.query_rep import QueryRep
+from brad.config.engine import Engine
+from brad.utils.table_sizer import TableSizer
 
 
 class Workload:
     """
     A representation of the workload to be considered during the blueprint
     planning process.
+
+    - List of analytical queries
+    - List of transactions (sampled) and the sample frequency
+    - Total dataset size
+    - Table sizes
+
+    The planner uses these values when comparing blueprints. The intention is
+    that these values can also be _forecasted_.
     """
 
     def __init__(self, templates: List[QueryTemplate]) -> None:
+        # NOTE: This will be removed
         self._templates = templates
+
+        self._analytical_queries: List[QueryRep] = []
+        self._transactional_queries: List[QueryRep] = []
+        self._transaction_sample_fraction = 0.01
+        self._dataset_size_mb = 0
+
+        # The size of a table on an engine.
+        self._table_sizes_mb: Dict[Tuple[str, Engine], int] = {}
 
     def templates(self) -> List[QueryTemplate]:
         return self._templates
+
+    async def populate_table_sizes_using_blueprint(
+        self, blueprint: Blueprint, table_sizer: TableSizer
+    ) -> None:
+        self._table_sizes_mb.clear()
+        for table, locations in blueprint.tables_with_locations():
+            for loc in locations:
+                self._table_sizes_mb[(table, loc)] = await table_sizer.table_size_mb(
+                    table, loc
+                )
+
+    def table_size_on_engine(self, table_name: str, location: Engine) -> Optional[int]:
+        try:
+            return self._table_sizes_mb[(table_name, location)]
+        except KeyError:
+            return None
---
+++ src/brad/utils/table_sizer.py
@@ -0,0 +1,72 @@
+import asyncio
+import boto3
+
+from brad.config.engine import Engine
+from brad.config.file import ConfigFile
+from brad.server.engine_connections import EngineConnections
+
+
+class TableSizer:
+    """
+    Utility class for retrieving the size of a table.
+    """
+
+    def __init__(self, engines: EngineConnections, config: ConfigFile) -> None:
+        self._engines = engines
+        self._config = config
+        self._s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=config.aws_access_key,
+            aws_secret_access_key=config.aws_access_key_secret,
+        )
+
+    async def table_size_mb(self, table_name: str, location: Engine) -> int:
+        if location == Engine.Aurora:
+            return await self._table_size_mb_aurora(table_name)
+        elif location == Engine.Athena:
+            return await self._table_size_mb_athena(table_name)
+        elif location == Engine.Redshift:
+            return await self._table_size_mb_redshift(table_name)
+        else:
+            raise RuntimeError(
+                "Unknown location {} for table {}".format(str(location), table_name)
+            )
+
+    async def _table_size_mb_athena(self, table_name: str) -> int:
+        # Format: s3://bucket/path/to/files/
+        parts = self._config.athena_s3_data_path.split("/")
+        bucket_name = parts[2]
+        data_prefix = "/".join(parts[3:])
+        table_prefix = "/" + data_prefix + table_name + "/"
+
+        # NOTE: This may be problematic when there are many objects in a prefix.
+        def run_inner():
+            total_size_bytes = 0
+            bucket = self._s3_client.Bucket(bucket_name)
+            for obj in bucket.objects.filter(Prefix=table_prefix):
+                total_size_bytes += obj.size
+            # `total_size` is in bytes.
+            return total_size_bytes
+
+        loop = asyncio.get_running_loop()
+        total_size_bytes = await loop.run_in_executor(None, run_inner)
+
+        return int(total_size_bytes / 1000 / 1000)
+
+    async def _table_size_mb_aurora(self, table_name: str) -> int:
+        query = "SELECT pg_table_size('{}')".format(table_name)
+        aurora = self._engines.get_connection(Engine.Aurora)
+        cursor = await aurora.cursor()
+        await cursor.execute(query)
+        result = await cursor.fetchone()
+        # The result is in bytes.
+        return int(int(result[0]) / 1000 / 1000)
+
+    async def _table_size_mb_redshift(self, table_name: str) -> int:
+        query = "SELECT size FROM svv_table_info WHERE table = '{}';".format(table_name)
+        redshift = self._engines.get_connection(Engine.Redshift)
+        cursor = await redshift.cursor()
+        await cursor.execute(query)
+        result = await cursor.fetchone()
+        table_size_mb = int(result[0])
+        return table_size_mb
