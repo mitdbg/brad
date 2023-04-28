f6f618db8e86d56cce70a6609c10df0d072f3f26
Markos Markakis <mmarkakis98@gmail.com>
2023-04-28 16:02:22 -0400

    Add moving average forecaster

--- src/brad/forecasting/moving_average_forecaster.py
+++ src/brad/forecasting/moving_average_forecaster.py
@@ -5,17 +5,22 @@ from datetime import datetime, timedelta
 
 
 class MovingAverageForecaster(Forecaster):
-    def __init__(self, df: pd.DataFrame, epoch_length: timedelta) -> None:
-        raise NotImplementedError
+    def __init__(
+        self, df: pd.DataFrame, epoch_length: timedelta, window_size: int = 5
+    ) -> None:
+        self._df = df
+        self._epoch_length = epoch_length
+        self._window_size = window_size
 
     def update_df_pointer(self, df: pd.DataFrame) -> None:
-        raise NotImplementedError
+        self._df = df
 
     # Returns empty list if `num_points` is <= 0
     def num_points(self, metric_id: str, num_points: int) -> List[float]:
-        raise NotImplementedError
+        return max(num_points, 0) * [self._df.tail(self._window_size).mean()[metric_id]]
 
     # `end_ts` is inclusive
     # Returns empty list if `end_ts` is sooner than one `_epoch_length` after the last entry in `_df`.
     def until(self, metric_id: str, end_ts: datetime) -> List[float]:
-        raise NotImplementedError
+        num_points = (end_ts - self._df.index[-1]) // self._epoch_length
+        return self.num_points(metric_id, num_points)
