2f7e255247ef34e1964f5d7e80558bde22ad732a
Markos Markakis <mmarkakis98@gmail.com>
2023-04-28 16:25:07 -0400

    Reformat `test_moving_average`

--- tests/test_moving_average_forecaster.py
+++ tests/test_moving_average_forecaster.py
@@ -1,12 +1,11 @@
 from brad.forecasting.moving_average_forecaster import MovingAverageForecaster
 import pandas as pd
-import numpy as np
 from datetime import datetime, timedelta
 
 
 def test_num_points():
     dataframe = pd.DataFrame(
-        {"a": [i for i in range(10)], "b": [i for i in range(10,20)]},
+        {"a": [i for i in range(10)], "b": [i for i in range(10, 20)]},
         index=pd.date_range("2022-01-01", "2022-01-10", freq="D", normalize=True),
     )
 
@@ -20,7 +19,7 @@ def test_num_points():
 
 def test_until():
     dataframe = pd.DataFrame(
-        {"a": [i for i in range(10)], "b": [i for i in range(10,20)]},
+        {"a": [i for i in range(10)], "b": [i for i in range(10, 20)]},
         index=pd.date_range("2022-01-01", "2022-01-10", freq="D", normalize=True),
     )
 
@@ -32,6 +31,10 @@ def test_until():
 
     assert f.until("a", datetime(year=2022, month=1, day=14)) == [7 for _ in range(4)]
 
-    assert f.until("a", datetime(year=2022, month=1, day=16, hour=5)) == [7 for _ in range(6)]
+    assert f.until("a", datetime(year=2022, month=1, day=16, hour=5)) == [
+        7 for _ in range(6)
+    ]
 
-    assert f.until("b", datetime(year=2022, month=1, day=17, hour=4)) == [17 for _ in range(7)]
+    assert f.until("b", datetime(year=2022, month=1, day=17, hour=4)) == [
+        17 for _ in range(7)
+    ]
