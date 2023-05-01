58c0ba3e75d9abaf7bae92e022294c5419ec8bd8
Geoffrey Yu <geoffxy@mit.edu>
2023-05-01 17:15:12 -0400

    Import changes

--- src/brad/planner/utils/parse_plan.py
+++ src/brad/planner/utils/parse_plan.py
@@ -2,11 +2,9 @@ import collections
 import re
 
 import numpy as np
-from tqdm import tqdm
 
-from cross_db_benchmark.benchmark_tools.generate_workload import LogicalOperator
-from cross_db_benchmark.benchmark_tools.postgres.plan_operator import PlanOperator
-from cross_db_benchmark.benchmark_tools.postgres.utils import plan_statistics
+from brad.planner.utils.plan_operator import PlanOperator
+from brad.planner.utils.postgres_utils import plan_statistics
 
 planning_time_regex = re.compile('planning time: (?P<planning_time>\d+.\d+) ms')
 ex_time_regex = re.compile('execution time: (?P<execution_time>\d+.\d+) ms')
@@ -104,9 +102,10 @@ def parse_plan(analyze_plan_tuples, analyze=True, parse=True):
     return root_operator, ex_time, planning_time
 
 
-def parse_plans(run_stats, min_runtime=100, max_runtime=30000, parse_baseline=False, cap_queries=None,
+def parse_plans(explain_str, min_runtime=100, max_runtime=30000, parse_baseline=False, cap_queries=None,
                 parse_join_conds=False, include_zero_card=False, explain_only=False, zero_card_min_runtime=None,
                 db_name=None, timeout_ms=None, target_path=None):
+
     # keep track of column statistics
     if zero_card_min_runtime is None:
         zero_card_min_runtime = min_runtime
@@ -114,203 +113,60 @@ def parse_plans(run_stats, min_runtime=100, max_runtime=30000, parse_baseline=Fa
     table_id_mapping = dict()
     partial_column_name_mapping = collections.defaultdict(set)
 
-    database_stats = run_stats.database_stats
-    # enrich column stats with table sizes
-    table_sizes = dict()
-    for table_stat in database_stats.table_stats:
-        table_sizes[table_stat.relname] = table_stat.reltuples
-
-    for i, column_stat in enumerate(database_stats.column_stats):
-        table = column_stat.tablename
-        column = column_stat.attname
-        column_stat.table_size = table_sizes[table]
-        column_id_mapping[(table, column)] = i
-        partial_column_name_mapping[column].add(table)
-
-    # similar for table statistics
-    for i, table_stat in enumerate(database_stats.table_stats):
-        table = table_stat.relname
-        table_id_mapping[table] = i
-
     # parse individual queries
     parsed_plans = []
-    sql_queries = []
     avg_runtimes = []
     no_tables = []
     no_filters = []
     op_perc = collections.defaultdict(int)
-    for query_no, q in enumerate(tqdm(run_stats.query_list)):
-        # either only parse explain part of query or skip entirely
-        curr_explain_only = explain_only
-        # do not parse timeout queries
-        if hasattr(q, 'timeout') and q.timeout:
-            continue
 
-        alias_dict = dict()
-        if not curr_explain_only:
-            if q.analyze_plans is None:
-                continue
+    # either only parse explain part of query or skip entirely
+    curr_explain_only = explain_only
 
-            if len(q.analyze_plans) == 0:
-                continue
+    alias_dict = dict()
 
-            # subqueries are currently not supported
-            analyze_str = ''.join([l[0] for l in q.verbose_plan])
-            if 'SubPlan' in analyze_str or 'InitPlan' in analyze_str:
-                continue
+    avg_runtime = 0
 
-            # subquery is empty due to logical constraints
-            if '->  Result  (cost=0.00..0.00 rows=0' in analyze_str:
-                continue
+    # only explain plan (not executed)
+    verbose_plan, _, _ = parse_plan(explain_str, analyze=False, parse=True)
+    verbose_plan.parse_lines_recursively(alias_dict=alias_dict, parse_baseline=parse_baseline,
+                                            parse_join_conds=parse_join_conds)
 
-            # check if it just initializes a plan
-            if isinstance(q.analyze_plans[0][0], list):
-                analyze_plan_string = ''.join(l[0] for l in q.analyze_plans[0])
-            else:
-                analyze_plan_string = ''.join(q.analyze_plans)
-            if init_plan_regex.search(analyze_plan_string) is not None:
-                continue
+    analyze_plan = verbose_plan
+    tables, filter_columns, operators = plan_statistics(analyze_plan)
 
-            # compute average execution and planning times
-            ex_times = []
-            planning_times = []
-            for analyze_plan in q.analyze_plans:
-                _, ex_time, planning_time = parse_plan(analyze_plan, analyze=True, parse=False)
-                ex_times.append(ex_time)
-                planning_times.append(planning_time)
-            avg_runtime = sum(ex_times) / len(ex_times)
+    analyze_plan.parse_columns_bottom_up(column_id_mapping, partial_column_name_mapping, table_id_mapping,
+                                            alias_dict=alias_dict)
+    analyze_plan.tables = tables
+    analyze_plan.num_tables = len(tables)
+    analyze_plan.plan_runtime = avg_runtime
 
-            # parse the plan as a tree
-            analyze_plan, _, _ = parse_plan(q.analyze_plans[0], analyze=True, parse=True)
+    def augment_no_workers(p, top_no_workers=0):
+        no_workers = p.plan_parameters.get('workers_planned')
+        if no_workers is None:
+            no_workers = top_no_workers
 
-            # parse information contained in operator nodes (different information in verbose and analyze plan)
-            analyze_plan.parse_lines_recursively(alias_dict=alias_dict, parse_baseline=parse_baseline,
-                                                 parse_join_conds=parse_join_conds)
+        p.plan_parameters['workers_planned'] = top_no_workers
 
-        # elif timeout:
-        #     avg_runtime = float(2 * max_runtime)
+        for c in p.children:
+            augment_no_workers(c, top_no_workers=no_workers)
 
-        else:
-            avg_runtime = 0
+    augment_no_workers(analyze_plan)
 
-        # only explain plan (not executed)
-        verbose_plan, _, _ = parse_plan(q.verbose_plan, analyze=False, parse=True)
-        verbose_plan.parse_lines_recursively(alias_dict=alias_dict, parse_baseline=parse_baseline,
-                                             parse_join_conds=parse_join_conds)
-
-        if not curr_explain_only:
-            # merge the plans with different information
-            analyze_plan.merge_recursively(verbose_plan)
-
-        else:
-            analyze_plan = verbose_plan
-
-        tables, filter_columns, operators = plan_statistics(analyze_plan)
-
-        analyze_plan.parse_columns_bottom_up(column_id_mapping, partial_column_name_mapping, table_id_mapping,
-                                             alias_dict=alias_dict)
-        analyze_plan.tables = tables
-        analyze_plan.num_tables = len(tables)
-        analyze_plan.plan_runtime = avg_runtime
-
-        def augment_no_workers(p, top_no_workers=0):
-            no_workers = p.plan_parameters.get('workers_planned')
-            if no_workers is None:
-                no_workers = top_no_workers
-
-            p.plan_parameters['workers_planned'] = top_no_workers
-
-            for c in p.children:
-                augment_no_workers(c, top_no_workers=no_workers)
-
-        augment_no_workers(analyze_plan)
-
-        if not curr_explain_only:
-            # check if result is None
-            if analyze_plan.min_card() == 0 and not include_zero_card:
-                print(f"parsed_query {query_no} has zero_card")
-                continue
-
-            elif analyze_plan.min_card() == 0 and avg_runtime < zero_card_min_runtime:
-                print(f"parsed_query {query_no} has zero_card and runtime too small")
-                continue
-
-            if min_runtime is not None and avg_runtime < min_runtime:
-                continue
-
-            if avg_runtime > max_runtime:
-                continue
-
-        # add joins for MSCN baseline
-        if parse_baseline:
-            join_conds = []
-            join_str = q.sql.split(' FROM ')[1]
-            # search for explicit join conditions
-            if ' JOIN ' in join_str:
-                if ' WHERE ' in join_str:
-                    join_str = join_str.split(' WHERE ')[0]
-
-                for p_join_str in join_str.split(' JOIN ')[1:]:
-                    p_join_str = p_join_str.split(' ON ')[1]
-                    join_cond = normalize_join_condition(p_join_str)
-                    join_conds.append(join_cond)
-            else:
-                # check for join comparisons in the where clause
-                for filter_m in join_columns_regex.finditer(join_str):
-                    p_join_str = filter_m.group()
-                    join_cond = normalize_join_condition(p_join_str)
-                    join_conds.append(join_cond)
-
-            tables, _, _ = plan_statistics(analyze_plan, skip_columns=True)
-            if len(tables) != len(join_conds) + 1:
-                print(f"No tables {len(tables)} != no join conds + 1 {len(join_conds) + 1}")
-            analyze_plan.join_conds = join_conds
-
-        # collect statistics
-        avg_runtimes.append(avg_runtime)
-        no_tables.append(len(tables))
-        for _, op in filter_columns:
-            op_perc[op] += 1
-        # log number of filters without counting AND, OR
-        no_filters.append(len([fc for fc in filter_columns if fc[0] is not None]))
-        
-        if "tables" in analyze_plan:
-            analyze_plan["tables"] = list(analyze_plan["tables"])
-        else:
-            analyze_plan["tables"] = []
-        
-        parsed_plans.append(analyze_plan)
-        sql_queries.append(q.sql)
-
-        if cap_queries is not None and len(parsed_plans) >= cap_queries:
-            print(f"Parsed {cap_queries} queries. Stopping parsing.")
-            break
-
-        if parse_baseline:
-            def list_columns(n, columns):
-                if n['operator'] != str(LogicalOperator.AND) and n['operator'] != str(LogicalOperator.OR):
-                    columns.append((n['column'], n['operator'], n['literal']))
-                for c in n['children']:
-                    list_columns(c, columns)
-
-            def list_col_rec(n, columns):
-                filter_column = n.plan_parameters.get('filter_columns')
-                if filter_column is not None:
-                    list_columns(filter_column, columns)
-                for c in n.children:
-                    list_col_rec(c, columns)
-
-            columns = []
-            list_col_rec(analyze_plan, columns)
-            if ' JOIN ' in q.sql:
-                if ' WHERE ' not in q.sql:
-                    exp_no_filter = 0
-                else:
-                    exp_no_filter = q.sql.split(' WHERE ')[1].count(' AND ') + 1
-
-                if not exp_no_filter <= len(columns):
-                    print(f"Warning: did not find enough filters exp_no_filter: {exp_no_filter} ({q.sql}), "
-                          f"columns: {columns}")
+    # collect statistics
+    avg_runtimes.append(avg_runtime)
+    no_tables.append(len(tables))
+    for _, op in filter_columns:
+        op_perc[op] += 1
+    # log number of filters without counting AND, OR
+    no_filters.append(len([fc for fc in filter_columns if fc[0] is not None]))
+    
+    if "tables" in analyze_plan:
+        analyze_plan["tables"] = list(analyze_plan["tables"])
+    else:
+        analyze_plan["tables"] = []
+    
+    parsed_plans.append(analyze_plan)
 
     # statistics in seconds
     print(f"Table statistics: "
@@ -324,13 +180,8 @@ def parse_plans(run_stats, min_runtime=100, max_runtime=30000, parse_baseline=Fa
           f"\n\tmedian: {np.median(avg_runtimes) / 1000:.2f}s"
           f"\n\tmax: {np.max(avg_runtimes) / 1000:.2f}s"
           f"\n\tmean: {np.mean(avg_runtimes) / 1000:.2f}s")
-    print(f"Parsed {len(parsed_plans)} plans ({len(run_stats.query_list) - len(parsed_plans)} had zero-cardinalities "
-          f"or were too fast).")
 
-    parsed_runs = dict(parsed_plans=parsed_plans,
-                       sql_queries=sql_queries,
-                       database_stats=database_stats,
-                       run_kwargs=run_stats.run_kwargs)
+    parsed_runs = dict(parsed_plans=parsed_plans)
 
     stats = dict(
         runtimes=str(avg_runtimes),
--- src/brad/planner/utils/utils.py
+++ src/brad/planner/utils/utils.py
@@ -1,103 +1,51 @@
-import numpy as np
-from balsa.util.simple_sql_parser import _GetJoinConds, _FormatJoinCond
+import json
+import os
+from types import SimpleNamespace
 
 
-def plan_statistics(plan_op, tables=None, filter_columns=None, operators=None, skip_columns=False, conv_to_dict=False):
-    if tables is None:
-        tables = set()
-    if operators is None:
-        operators = set()
-    if filter_columns is None:
-        filter_columns = set()
+def dumper(obj):
+    try:
+        return obj.toJSON()
+    except:
+        return obj.__dict__
 
-    params = plan_op.plan_parameters
-
-    if conv_to_dict:
-        params = vars(params)
-
-    if 'table' in params:
-        tables.add(params['table'])
-    if 'op_name' in params:
-        operators.add(params['op_name'])
-    if 'filter_columns' in params and not skip_columns:
-        list_columns(params['filter_columns'], filter_columns)
-
-    for c in plan_op.children:
-        plan_statistics(c, tables=tables, filter_columns=filter_columns, operators=operators, skip_columns=skip_columns,
-                        conv_to_dict=conv_to_dict)
-
-    return tables, filter_columns, operators
+def load_schema_json(dataset):
+    schema_path = os.path.join('cross_db_benchmark/datasets/', dataset, 'schema.json')
+    assert os.path.exists(schema_path), f"Could not find schema.json ({schema_path})"
+    return load_json(schema_path)
 
 
-def child_prod(p, feature_name, default=1):
-    child_feat = [c.plan_parameters.get(feature_name) for c in p.children
-                  if c.plan_parameters.get(feature_name) is not None]
-    if len(child_feat) == 0:
-        return default
-    return np.prod(child_feat)
+def save_schema_json(dataset, schema):
+    schema_path = os.path.join('cross_db_benchmark/datasets/', dataset, 'schema.json')
+    assert os.path.exists(schema_path), f"Could not find schema.json ({schema_path})"
+    with open(schema_path, "w") as f:
+        json.dump(schema, f, default=dumper)
 
 
-def list_columns(n, columns):
-    columns.add((n.column, n.operator))
-    for c in n.children:
-        list_columns(c, columns)
+def load_column_statistics(dataset, namespace=True):
+    path = os.path.join('cross_db_benchmark/datasets/', dataset, 'column_statistics.json')
+    assert os.path.exists(path), f"Could not find file ({path})"
+    return load_json(path, namespace=namespace)
 
 
-def get_leaf_nodes(root, leaves):
-    if "children" not in root or len(root["children"]) == 0:
-        leaves.append(root["plan_parameters"])
-    else:
-        for child in root["children"]:
-            get_leaf_nodes(child, leaves)
+def load_string_statistics(dataset, namespace=True):
+    path = os.path.join('cross_db_benchmark/datasets/', dataset, 'string_statistics.json')
+    assert os.path.exists(path), f"Could not find file ({path})"
+    return load_json(path, namespace=namespace)
 
 
-def getJoinConds(alias_dict, sql, table_id_mapping=None, column_id_mapping=None):
-    joins, quotation = _GetJoinConds(sql)
-    join_conds = dict()
-    t1_alias = None
-    t2_alias = None
-    for (t1, k1, t2, k2) in joins:
-        clause = _FormatJoinCond((t1, k1, t2, k2), quotation)
-        if alias_dict is not None:
-            if t1 in alias_dict:
-                t1_alias = t1
-                t1 = alias_dict[t1]
-            if t2 in alias_dict:
-                t2_alias = t2
-                t2 = alias_dict[t2]
-        if table_id_mapping and t1 in table_id_mapping and t2 in table_id_mapping:
-            t1_id = table_id_mapping[t1]
-            t2_id = table_id_mapping[t2]
+def load_json(path, namespace=True):
+    with open(path) as json_file:
+        if namespace:
+            json_obj = json.load(json_file, object_hook=lambda d: SimpleNamespace(**d))
         else:
-            t1_id = t1
-            t2_id = t2
-        if column_id_mapping and (t1, k1) in column_id_mapping and (t2, k2) in column_id_mapping:
-            k1_id = column_id_mapping[(t1, k1)]
-            k2_id = column_id_mapping[(t2, k2)]
-        else:
-            k1_id = k1
-            k2_id = k2
-        join_conds[clause] = ([t1_id, k1_id, t2_id, k2_id], [t1_alias, t2_alias])
-    return join_conds
+            json_obj = json.load(json_file)
+    return json_obj
 
 
-def getFilters(plan, return_text=False):
-    leaf_nodes = []
-    get_leaf_nodes(plan, leaf_nodes)
-    filter_texts = dict()
-    filter_nodes = dict()
-    for leaf in leaf_nodes:
-        if 'table' in leaf:
-            # this is a scan node, maybe this if statement is not needed
-            if 'alias' in leaf and leaf['alias'] is not None:
-                scan_id = leaf['alias']
-            else:
-                scan_id = leaf['table']
-            if return_text:
-                filter_text = None
-                if 'filter_text' in leaf and leaf['filter_text'] is not None and len(leaf['filter_text']) != 0:
-                    filter_text = leaf['filter_text']
-                filter_texts[scan_id] = filter_text
-            filter_nodes[scan_id] = leaf
-    return filter_nodes, filter_texts
-
+def load_schema_sql(dataset, sql_filename):
+    sql_path = os.path.join('cross_db_benchmark/datasets/', dataset, 'schema_sql', sql_filename)
+    assert os.path.exists(sql_path), f"Could not find schema.sql ({sql_path})"
+    with open(sql_path, 'r') as file:
+        data = file.read().replace('\n', '')
+    return data
