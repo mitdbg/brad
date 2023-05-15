66e3690044d47bd721515317d2d307bfa3acb473
wuziniu <wuziniu2015@gmail.com>
2023-05-15 14:19:00 -0400

    adding router rule_based with imdb catalog + exposing port in docker start.sh

--- src/brad/routing/rule_based.py
+++ src/brad/routing/rule_based.py
@@ -1,15 +1,35 @@
-from typing import List, Optional, Mapping
+import os.path
+import json
+from typing import List, Optional, Mapping, Any
+from importlib.resources import files, as_file
 
 from brad.blueprint import Blueprint
 from brad.config.engine import Engine
 from brad.server.blueprint_manager import BlueprintManager
 from brad.daemon.monitor import Monitor
+import brad.routing
 from brad.routing import Router
 from brad.query_rep import QueryRep
+from brad.server.session import SessionManager
 
 
 class RuleBasedParams(object):
     def __init__(self):
+        # Ideal location threshold
+        ideal_location_upper_limit: Mapping[str, Optional[float]] = dict()
+        ideal_location_upper_limit["aurora_num_table"] = 6
+        ideal_location_upper_limit["aurora_table_size_max"] = 5e7
+        ideal_location_upper_limit["aurora_table_size_sum"] = 2e8
+        ideal_location_upper_limit["aurora_table_ncolumn_max"] = 15
+        ideal_location_upper_limit["aurora_table_ncolumn_sum"] = 40
+        ideal_location_upper_limit["redshift_num_table"] = 10
+        ideal_location_upper_limit["redshift_table_size_max"] = 5e9
+        ideal_location_upper_limit["redshift_table_size_sum"] = 2e11
+        self.ideal_location_upper_limit = ideal_location_upper_limit
+        ideal_location_lower_limit: Mapping[str, Optional[float]] = dict()
+        ideal_location_lower_limit["redshift_num_table"] = 3.0
+        self.ideal_location_lower_limit = ideal_location_lower_limit
+
         # Aurora metric threshold
         aurora_parameters_upper_limit: Mapping[str, Optional[float]] = dict()
         aurora_parameters_upper_limit["aurora_WRITER_CPUUtilization_Average"] = 50.0
@@ -38,15 +58,89 @@ class RuleBased(Router):
         blueprint_mgr: Optional[BlueprintManager] = None,
         blueprint: Optional[Blueprint] = None,
         monitor: Optional[Monitor] = None,
+        catalog: Optional[Mapping[str, Mapping[str, Any]]] = None,
+        decision_tree: bool = False,
         deterministic: bool = True,
     ):
         self._blueprint_mgr = blueprint_mgr
         self._blueprint = blueprint
         self._monitor = monitor
+        # catalog contains all tables' number of rows and columns
+        self._catalog = catalog
+        if self._catalog is None:
+            _catalog_file = files(brad.routing).joinpath("imdb_catalog.json")
+            with as_file(_catalog_file) as file:
+                if os.path.exists(file):
+                    with open(file, "r") as f:
+                        self._catalog = json.load(f)
+        # use decision tree instead of rules
+        self._decision_tree = decision_tree
         # deterministic routing guarantees the same decision for the same query and should be used online
         # non-determinism will be used for offline training data exploration (not implemented)
-        self.deterministic = deterministic
-        self.params = RuleBasedParams()
+        self._deterministic = deterministic
+        self._params = RuleBasedParams()
+
+    async def recollect_catalog(self, sessions: SessionManager) -> None:
+        # recollect catalog stats; happens every maintenance window
+        if self._catalog is None:
+            self._catalog = dict()
+        session_id, _ = await sessions.create_new_session()
+        session = sessions.get_session(session_id)
+        # Since only Aurora handles txn, we only need connection to Aurora
+        connection = session.engines.get_connection(Engine.Aurora)
+        cursor = await connection.cursor()
+
+        if self._blueprint is not None:
+            blueprint = self._blueprint
+        else:
+            assert self._blueprint_mgr is not None
+            blueprint = self._blueprint_mgr.get_blueprint()
+
+        indexes_sql = (
+            "SELECT tablename, indexname, indexdef FROM pg_indexes WHERE schemaname = 'public' "
+            "ORDER BY tablename, indexname;"
+        )
+        await cursor.execute(indexes_sql)
+        all_indexes_raw = await cursor.fetchall()
+        all_indexes = dict()
+        for index in all_indexes_raw:
+            brad_table_name = index[0]
+            if brad_table_name not in all_indexes:
+                all_indexes[brad_table_name] = []
+            all_indexes[brad_table_name].append(index)
+
+        for table_name in blueprint.table_locations():
+            location = blueprint.table_locations()[table_name]
+            if Engine.Aurora in location:
+                # the following syntax only works for Aurora, we also assume all txn happens in Aurora
+                # so if a table is not on Aurora, it will have no change.
+                nrow_sql = f"SELECT COUNT(*) FROM {table_name};"
+                await cursor.execute(nrow_sql)
+                nrow = await cursor.fetchone()
+                ncol_sql = f"""SELECT COUNT(*)
+                                  FROM INFORMATION_SCHEMA.COLUMNS
+                                  WHERE table_catalog = '{blueprint.schema_name()}'
+                                  AND table_name = '{table_name}';
+                           """
+                await cursor.execute(ncol_sql)
+                ncol = await cursor.fetchone()
+
+                brad_table_name = table_name + "_brad_source"
+                table_indexes = []
+                table_PKs = []
+                if brad_table_name in all_indexes:
+                    for index in all_indexes[brad_table_name]:
+                        column_name = index[-1].split("(")[-1].split(")")[0]
+                        table_indexes.append(column_name)
+                        if "_brad_source_pkey" in index[1]:
+                            table_PKs.append(column_name)
+                self._catalog[table_name] = {
+                    "nrow": nrow[0],
+                    "ncol": ncol[0],
+                    "indexes": table_indexes,
+                    "PKs": table_PKs,
+                }
+        await sessions.end_session(session_id)
 
     def check_engine_state(
         self, engine: Engine, sys_metric: Mapping[str, float]
@@ -54,27 +148,27 @@ class RuleBased(Router):
         # We define a set of rules to judge whether the current system is overloaded
         not_overloaded = True
         if engine == Engine.Aurora:
-            for param in self.params.aurora_parameters_upper_limit:
-                limit = self.params.aurora_parameters_upper_limit[param]
+            for param in self._params.aurora_parameters_upper_limit:
+                limit = self._params.aurora_parameters_upper_limit[param]
                 if limit and param in sys_metric and sys_metric[param] > limit:
                     not_overloaded = False
                     break
             if not_overloaded:
-                for param in self.params.aurora_parameters_lower_limit:
-                    limit = self.params.aurora_parameters_lower_limit[param]
+                for param in self._params.aurora_parameters_lower_limit:
+                    limit = self._params.aurora_parameters_lower_limit[param]
                     if limit and param in sys_metric and sys_metric[param] < limit:
                         not_overloaded = False
                         break
 
         if engine == Engine.Redshift:
-            for param in self.params.redshift_parameters_upper_limit:
-                limit = self.params.redshift_parameters_upper_limit[param]
+            for param in self._params.redshift_parameters_upper_limit:
+                limit = self._params.redshift_parameters_upper_limit[param]
                 if limit and param in sys_metric and sys_metric[param] > limit:
                     not_overloaded = False
                     break
             if not_overloaded:
-                for param in self.params.redshift_parameters_lower_limit:
-                    limit = self.params.redshift_parameters_lower_limit[param]
+                for param in self._params.redshift_parameters_lower_limit:
+                    limit = self._params.redshift_parameters_lower_limit[param]
                     if limit and param in sys_metric and sys_metric[param] < limit:
                         not_overloaded = False
                         break
@@ -119,14 +213,75 @@ class RuleBased(Router):
             return locations[0]
         else:
             ideal_location_rank: List[Engine] = []
-            # Todo: include infos on table size (can be stored in this class and updated at each maintainence window)
-            # Todo: add more rules (e.g. index, simple selective)
-            if len(query.tables()) >= 6:
-                ideal_location_rank = [Engine.Athena, Engine.Redshift, Engine.Aurora]
-            elif len(query.tables()) >= 3:
-                ideal_location_rank = [Engine.Redshift, Engine.Athena, Engine.Aurora]
-            else:
+            # Todo(ziniu): include index or simple selectivity
+            touched_tables = query.tables()
+            if (
+                len(touched_tables)
+                < self._params.ideal_location_lower_limit["redshift_num_table"]
+            ):
                 ideal_location_rank = [Engine.Aurora, Engine.Redshift, Engine.Athena]
+            elif (
+                len(touched_tables)
+                <= self._params.ideal_location_upper_limit["aurora_num_table"]
+            ):
+                ideal_location_rank = [Engine.Redshift, Engine.Aurora, Engine.Athena]
+                if self._catalog:
+                    n_rows = []
+                    n_cols = []
+                    for table_name in query.tables():
+                        if table_name in self._catalog:
+                            n_rows.append(self._catalog[table_name]["nrow"])
+                            n_cols.append(self._catalog[table_name]["ncol"])
+                    if (
+                        max(n_rows)
+                        < self._params.ideal_location_upper_limit[
+                            "aurora_table_size_max"
+                        ]
+                        and sum(n_rows)
+                        < self._params.ideal_location_upper_limit[
+                            "aurora_table_size_sum"
+                        ]
+                        and max(n_cols)
+                        < self._params.ideal_location_upper_limit[
+                            "aurora_table_ncolumn_max"
+                        ]
+                        and sum(n_cols)
+                        < self._params.ideal_location_upper_limit[
+                            "aurora_table_ncolumn_sum"
+                        ]
+                    ):
+                        ideal_location_rank = [
+                            Engine.Aurora,
+                            Engine.Redshift,
+                            Engine.Athena,
+                        ]
+            elif (
+                len(touched_tables)
+                <= self._params.ideal_location_upper_limit["redshift_num_table"]
+            ):
+                ideal_location_rank = [Engine.Redshift, Engine.Athena, Engine.Aurora]
+                if self._catalog:
+                    n_rows = []
+                    for table_name in query.tables():
+                        if table_name in self._catalog:
+                            n_rows.append(self._catalog[table_name]["nrow"])
+                    if (
+                        max(n_rows)
+                        > self._params.ideal_location_upper_limit[
+                            "redshift_table_size_max"
+                        ]
+                        and sum(n_rows)
+                        > self._params.ideal_location_upper_limit[
+                            "redshift_table_size_sum"
+                        ]
+                    ):
+                        ideal_location_rank = [
+                            Engine.Athena,
+                            Engine.Redshift,
+                            Engine.Aurora,
+                        ]
+            else:
+                ideal_location_rank = [Engine.Athena, Engine.Redshift, Engine.Aurora]
 
             if self._monitor is None:
                 for loc in ideal_location_rank:
@@ -135,7 +290,7 @@ class RuleBased(Router):
                 # This should be unreachable since len(locations) > 0.
                 assert False
             else:
-                # Todo: this can be stored in this class to reduce latency
+                # Todo(Ziniu): this can be stored in this class to reduce latency
                 raw_sys_metric = self._monitor.read_k_most_recent(k=1)
                 col_name = list(raw_sys_metric.columns)
                 col_value = list(raw_sys_metric.values)[0]
