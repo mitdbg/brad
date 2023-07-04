25fa776a094870d780e7fa81a89303f127042c6a
Geoffrey Yu <geoffxy@mit.edu>
2023-07-04 18:49:28 -0400

    Switch to system load metric scaling on Aurora (#187)

--- src/brad/planner/metrics.py
+++ src/brad/planner/metrics.py
@@ -2,7 +2,13 @@ from collections import namedtuple
 from brad.daemon.monitor import Monitor
 
 Metrics = namedtuple(
-    "Metrics", ["redshift_cpu_avg", "aurora_cpu_avg", "buffer_hit_pct_avg"]
+    "Metrics",
+    [
+        "redshift_cpu_avg",
+        "aurora_cpu_avg",
+        "buffer_hit_pct_avg",
+        "aurora_load_minute_avg",
+    ],
 )
 
 
@@ -44,13 +50,14 @@ class MetricsFromMonitor(MetricsProvider):
             )
 
         if metrics.empty:
-            return Metrics(1.0, 1.0, 100.0)
+            return Metrics(1.0, 1.0, 100.0, 1.0)
 
         redshift_cpu = metrics[_RELEVANT_METRICS["redshift_cpu_avg"]].iloc[0]
         aurora_cpu = metrics[_RELEVANT_METRICS["aurora_cpu_avg"]].iloc[0]
         hit_pct = metrics[_RELEVANT_METRICS["buffer_hit_pct_avg"]].iloc[0]
 
-        return Metrics(redshift_cpu, aurora_cpu, hit_pct)
+        # TODO: Retrieve performance insights metrics.
+        return Metrics(redshift_cpu, aurora_cpu, hit_pct, aurora_load_minute_avg=1.0)
 
 
 _RELEVANT_METRICS = {
