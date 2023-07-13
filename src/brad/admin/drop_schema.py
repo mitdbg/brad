9b0d45b9f1c53de821c300c4ac80dc691dfafeee
Geoffrey Yu <geoffxy@mit.edu>
2023-07-13 11:05:14 -0400

    Ensure we drop other serialized state too

--- src/brad/admin/drop_schema.py
+++ src/brad/admin/drop_schema.py
@@ -1,9 +1,10 @@
 import logging
-import pyodbc
 
 from brad.asset_manager import AssetManager
 from brad.config.engine import Engine
 from brad.config.file import ConfigFile
+from brad.routing.policy import RoutingPolicy
+from brad.routing.tree_based.forest_router import ForestRouter
 from brad.server.blueprint_manager import BlueprintManager
 from brad.server.engine_connections import EngineConnections
 
@@ -42,13 +43,23 @@ def drop_schema(args):
     aurora = cxns.get_connection(Engine.Aurora).cursor_sync()
     athena = cxns.get_connection(Engine.Athena).cursor_sync()
 
+    logger.info("Starting the schema drop...")
+
     # 4. Drop the underlying "databases" if they exist.
     athena.execute_sync("DROP DATABASE IF EXISTS {} CASCADE".format(args.schema_name))
     aurora.execute_sync("DROP DATABASE IF EXISTS {}".format(args.schema_name))
     try:
         redshift.execute_sync("DROP DATABASE {}".format(args.schema_name))
-    except pyodbc.Error:
+    except:  # pylint: disable=bare-except
         # Ignore the error if a database does not exist.
         logger.exception("Exception when dropping Redshift database.")
+
+    # 5. Drop any serialized routers.
+    ForestRouter.static_drop_model_sync(
+        args.schema_name, RoutingPolicy.ForestTableSelectivity, assets
+    )
+    ForestRouter.static_drop_model_sync(
+        args.schema_name, RoutingPolicy.ForestTablePresence, assets
+    )
 
     logger.info("Done!")
