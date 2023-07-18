3d771f0e5b4a3aef192ccab8f1ad2e2f973c603a
Geoffrey Yu <geoffxy@mit.edu>
2023-07-18 16:45:41 -0400

    Add additional helpers, reorganize code

---
+++ workloads/IMDB_extended/transactions/database.py
@@ -0,0 +1,57 @@
+import pyodbc
+
+from brad.grpc_client import BradGrpcClient, RowList
+
+
+class Database:
+    def execute_sync(self, query: str) -> RowList:
+        raise NotImplementedError
+
+    def commit_sync(self) -> None:
+        raise NotImplementedError
+
+    def rollback_sync(self) -> None:
+        raise NotImplementedError
+
+    def close_sync(self) -> None:
+        raise NotImplementedError
+
+
+class PyodbcDatabase(Database):
+    def __init__(self, connection) -> None:
+        self._conn = connection
+        self._cursor = self._conn.cursor()
+
+    def execute_sync(self, query: str) -> RowList:
+        self._cursor.execute(query)
+        try:
+            rows = self._cursor.fetchall()
+            return list(rows)
+        except pyodbc.ProgrammingError:
+            return []
+
+    def commit_sync(self) -> None:
+        self._cursor.execute("COMMIT")
+
+    def rollback_sync(self) -> None:
+        self._cursor.execute("ROLLBACK")
+
+    def close_sync(self) -> None:
+        self._conn.close()
+
+
+class BradDatabase(Database):
+    def __init__(self, brad_client: BradGrpcClient) -> None:
+        self._brad = brad_client
+
+    def execute_sync(self, query: str) -> RowList:
+        return self._brad.run_query_json(query)
+
+    def commit_sync(self) -> None:
+        self._brad.run_query_ignore_results("COMMIT")
+
+    def rollback_sync(self) -> None:
+        self._brad.run_query_ignore_results("ROLLBACK")
+
+    def close_sync(self) -> None:
+        self._brad.close()
