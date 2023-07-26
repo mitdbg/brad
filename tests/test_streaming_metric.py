b59a43b68ae1a5c98b0a328f4623ce80ea7cf094
Geoffrey Yu <geoffxy@mit.edu>
2023-07-26 18:38:36 -0400

    Fix windowed average implementation and add tests

--- tests/test_streaming_metric.py
+++ tests/test_streaming_metric.py
@@ -24,9 +24,18 @@ def test_multiple():
     sm = StreamingMetric[float]()
     for val, ts in get_value_stream(start):
         sm.add_sample(val, ts)
-    val = sm.average_since(start + timedelta(seconds=45))
+
+    val = sm.average_since(start + timedelta(seconds=15))
     # Average of 10.0 and 20.0
+    # We exclude the first datapoint because each sample covers the previous
+    # time window.
     assert val == 15.0
+
+    val = sm.average_since(start + timedelta(seconds=45))
+    # Average of 20.0
+    # We exclude the first two datapoints because each sample covers the
+    # previous time window.
+    assert val == 20.0
 
 
 def test_all():
@@ -43,4 +52,60 @@ def test_all():
     assert val == 11.0
 
     val = sm.average_since(start + timedelta(seconds=10))
-    assert val == 11.0
+    # We exclude the first datapoint because each sample covers the previous
+    # time window.
+    assert val == 15.0
+
+
+def test_window_average():
+    start = datetime(year=2023, month=7, day=26)
+    sm = StreamingMetric[float]()
+    sm.add_sample(3.0, start)
+    sm.add_sample(10.0, start + timedelta(seconds=10))
+    sm.add_sample(20.0, start + timedelta(seconds=20))
+    sm.add_sample(30.0, start + timedelta(seconds=30))
+    sm.add_sample(40.0, start + timedelta(seconds=40))
+
+    # Window within one sample.
+    val = sm.average_in_window(
+        start + timedelta(seconds=1), start + timedelta(seconds=2)
+    )
+    assert val == 10.0
+
+    # Window spanning two samples.
+    val = sm.average_in_window(
+        start + timedelta(seconds=1), start + timedelta(seconds=11)
+    )
+    assert val == 15.0
+
+    # Window spanning three samples.
+    val = sm.average_in_window(
+        start + timedelta(seconds=1), start + timedelta(seconds=21)
+    )
+    assert val == (10.0 + 20.0 + 30.0) / 3
+
+    # Window beyond end.
+    val = sm.average_in_window(
+        start + timedelta(seconds=1), start + timedelta(seconds=41)
+    )
+    assert val == (10.0 + 20.0 + 30.0 + 40.0) / 4
+
+    # Whole range.
+    val = sm.average_in_window(
+        start - timedelta(seconds=1), start + timedelta(seconds=42)
+    )
+    assert val == (3.0 + 10.0 + 20.0 + 30.0 + 40.0) / 5
+
+    # Before start.
+    val = sm.average_in_window(
+        start - timedelta(seconds=10), start - timedelta(seconds=1)
+    )
+    assert val == 3.0
+
+    # After end.
+    val = sm.average_in_window(
+        start + timedelta(seconds=45), start + timedelta(seconds=50)
+    )
+    assert val == 0.0
+
+
