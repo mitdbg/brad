183c19bd9d62cbd7872eb6986b6e8b4191e63fa6
Geoffrey Yu <geoffxy@mit.edu>
2023-08-01 15:07:09 -0400

    Touch the theatres table during transactions

--- workloads/IMDB_extended/workload_utils/transaction_worker.py
+++ workloads/IMDB_extended/workload_utils/transaction_worker.py
@@ -130,18 +130,27 @@ class TransactionWorker:
             db.rollback_sync()
             return False
 
-    def purchase_tickets(self, db: Database) -> bool:
+    def purchase_tickets(self, db: Database, select_using_name: bool) -> bool:
         """
         Represents a user buying tickets for a specific showing.
 
-        - Select theatre by id
+        - Select theatre (by name or id)
         - Select showing by theatre id and date
         - Insert into `ticket_order`
         - Update the `showing` entry
         """
 
-        # 1. Select a random theatre id.
-        theatre_id = self.prng.randint(self.min_theatre_id, self.max_theatre_id)
+        # 1. Select a random theatre number.
+        theatre_num = self.prng.randint(self.min_theatre_id, self.max_theatre_id)
+
+        if select_using_name:
+            results = db.execute_sync(
+                f"SELECT id FROM theatres WHERE name = 'Theatre #{theatre_num}'"
+            )
+            theatre_id = results[0][0]
+        else:
+            # By design, the theatre number is equal to the ID.
+            theatre_id = theatre_num
 
         try:
             # Start the transaction.
