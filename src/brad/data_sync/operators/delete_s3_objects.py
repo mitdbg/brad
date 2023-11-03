fb4198288624085713bfe88f26327acd0cebd4aa
Markos Markakis <mmarkakis98@gmail.com>
2023-11-03 17:34:53 -0400

    Delete operator supports absolute path

--- src/brad/data_sync/operators/delete_s3_objects.py
+++ src/brad/data_sync/operators/delete_s3_objects.py
@@ -11,29 +11,32 @@ logger = logging.getLogger(__name__)
 class DeleteS3Objects(Operator):
     """
     Deletes objects from S3. Used to clean up extracted data after finishing a
-    sync.
+    sync or a table movement.
     """
 
-    def __init__(self, s3_paths: List[str]) -> None:
+    def __init__(self, s3_paths: List[str], paths_are_relative: bool = True) -> None:
         """
-        NOTE: All S3 paths are relative to the extract path, specified in the
+        NOTE: All S3 paths can be relative to the extract path, specified in the
         configuration.
         """
         super().__init__()
         self._s3_paths = s3_paths
+        self._paths_are_relative = paths_are_relative
 
     def __repr__(self) -> str:
         return "".join(["DeleteS3Objects(<", str(len(self._s3_paths)), " objects>)"])
 
     async def execute(self, ctx: ExecutionContext) -> "Operator":
         loop = asyncio.get_running_loop()
-        for relative_s3_path in self._s3_paths:
+        for s3_path in self._s3_paths:
             await loop.run_in_executor(
                 None,
                 self._delete_object_sync,
                 ctx.s3_client(),
                 ctx.s3_bucket(),
-                "{}{}".format(ctx.s3_path(), relative_s3_path),
+                "{}{}".format(ctx.s3_path(), s3_path)
+                if self._paths_are_relative
+                else s3_path,
             )
         return self
 
@@ -42,3 +45,4 @@ class DeleteS3Objects(Operator):
             Bucket=s3_bucket,
             Key=full_s3_path,
         )
+        logger.debug("Deleted S3 object: %s", full_s3_path)
