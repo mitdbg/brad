13ec7b5def8ef81dc819adc08e6c27d077af9e84
Markos Markakis <mmarkakis98@gmail.com>
2023-11-03 13:33:58 -0400

    Add operators to drop aurora triggers and views

---
+++ src/brad/data_sync/operators/drop_aurora_triggers.py
@@ -0,0 +1,66 @@
+import logging
+from typing import List
+
+from .operator import Operator
+from brad.data_sync.execution.context import ExecutionContext
+from brad.config.strings import *
+
+logger = logging.getLogger(__name__)
+
+
+class DropAuroraTriggers(Operator):
+    """
+    Drops the Aurora triggers and trigger functions associated with specific tables.
+    """
+
+    def __init__(self, table_names: List[str]) -> None:
+        super().__init__()
+        self._table_names = table_names
+
+    def __repr__(self) -> str:
+        return "".join(
+            [
+                "DropAuroraTriggers(",
+                ", ".join(self._table_names),
+                ")",
+            ]
+        )
+
+    async def execute(self, ctx: ExecutionContext) -> "Operator":
+        drop_trigger_template = "DROP TRIGGER {} ON {}"
+        drop_trigger_fn_template = "DROP FUNCTION {}"
+
+        for table in self._table_names:
+            # Drop update trigger
+            q = drop_trigger_template.format(
+                update_trigger_name(table), source_table_name(table)
+            )
+            logger.debug("Running on Aurora: %s", q)
+            aurora = await ctx.aurora()
+            await aurora.execute(q)
+
+            # Drop delete trigger
+            q = drop_trigger_template.format(
+                delete_trigger_name(table), source_table_name(table)
+            )
+            logger.debug("Running on Aurora: %s", q)
+            aurora = await ctx.aurora()
+            await aurora.execute(q)
+
+            # Drop update trigger function
+            q = drop_trigger_fn_template.format(
+                update_trigger_function_name(table)
+            )
+            logger.debug("Running on Aurora: %s", q)
+            aurora = await ctx.aurora()
+            await aurora.execute(q)
+
+            # Drop delete trigger function
+            q = drop_trigger_fn_template.format(
+                delete_trigger_function_name(table)
+            )
+            logger.debug("Running on Aurora: %s", q)
+            aurora = await ctx.aurora()
+            await aurora.execute(q)
+
+        return self
