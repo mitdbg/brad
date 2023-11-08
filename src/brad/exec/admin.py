52dc343b5c72e379550c63608f9f7d9a1c5d13d5
Geoffrey Yu <geoffxy@mit.edu>
2023-11-08 15:58:38 -0500

    Add utility to restore to historical blueprint for experiment purposes

--- src/brad/exec/admin.py
+++ src/brad/exec/admin.py
@@ -11,6 +11,7 @@ import brad.admin.train_router as train_router
 import brad.admin.workload_logs as workload_logs
 import brad.admin.run_on as run_on
 import brad.admin.control as control
+import brad.admin.restore_blueprint as restore_blueprint
 
 logger = logging.getLogger(__name__)
 
@@ -35,6 +36,7 @@ def register_command(subparsers) -> None:
     workload_logs.register_admin_action(admin_subparsers)
     run_on.register_admin_action(admin_subparsers)
     control.register_admin_action(admin_subparsers)
+    restore_blueprint.register_admin_action(admin_subparsers)
     parser.set_defaults(func=main)
 
 
