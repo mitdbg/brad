d28740d0705cf4203eafe6c4c7e597fd6d1a535c
Geoffrey Yu <geoffxy@mit.edu>
2023-11-20 14:39:49 -0500

    Revise comparator based on discussions

--- src/brad/planner/compare/cost_with_benefit.py
+++ src/brad/planner/compare/cost_with_benefit.py
@@ -7,64 +7,85 @@ from .blueprint import ComparableBlueprint
 from .function import BlueprintComparator
 
 
-def best_cost_under_perf_ceilings_with_payoff(
-    max_query_latency_s: float,
+def best_cost_under_perf_ceilings_with_benefit_horizon(
+    max_query_p90_latency_s: float,
     max_txn_p90_latency_s: float,
-    payoff_period: timedelta,
-    payoff_penalty: float,
+    curr_query_p90_latency_s: float,
+    curr_txn_p90_latency_s: float,
     curr_hourly_cost: float,
-    curr_blueprint_in_penalty_region: bool,
+    benefit_horizon: timedelta,
 ) -> BlueprintComparator:
     def is_better_than(left: ComparableBlueprint, right: ComparableBlueprint) -> bool:
-        # Check transactional latency ceilings first.
+        # Transactional latency ceilings (feasibility check).
         result = _txn_p90_ceiling(left, right, max_txn_p90_latency_s)
         if result is not None:
             return result
 
-        # Query latency ceilings.
-        result = _query_ceiling(left, right, max_query_latency_s)
+        # Query latency ceilings (feasibility check).
+        result = _query_p90_ceiling(left, right, max_query_p90_latency_s)
         if result is not None:
             return result
 
-        # Transition times.
-        result = _transition_under_payoff_period(left, right, payoff_period)
+        # Transition times (feasibility check).
+        result = _transition_under_benefit_horizon(left, right, benefit_horizon)
         if result is not None:
             return result
 
-        if not curr_blueprint_in_penalty_region:
-            curr_cost_value = curr_hourly_cost
-        else:
-            curr_cost_value = payoff_penalty
+        # We compute a scalar score for each blueprint (lower is better):
+        #
+        # Score = Penalty * C_0 * T + C * (B - T)
+        # Penalty = 1 + max(0, max(curr_query_p90 / max_query_p90, curr_txn_p90 / max_txn_p90) - threshold)
+        # Threshold = 0.8 (hyperparameter)
+        #
+        # Notation:
+        # C_0 is the operating cost of the current blueprint ($/h)
+        # C is the operating cost of the proposed blueprint ($/h)
+        # T is the transition time (h)
+        # B is the benefit horizon (h)
+        #
+        # Intuition: The penalty reweighs the first term when we are close to
+        # exceeding the performance objectives to encourage selecting blueprints
+        # that are faster to transition to.
+        penalty_multiplier = _compute_penalty_multiplier(
+            max_query_p90_latency_s,
+            max_txn_p90_latency_s,
+            curr_query_p90_latency_s,
+            curr_txn_p90_latency_s,
+            0.8,
+        )
 
-        left_score = _compute_payoff_cost(
+        left_score = _compute_scalar_score(
             left.get_transition_time_s(),
             left.get_transition_cost(),
-            curr_cost_value,
+            curr_hourly_cost,
             left.get_operational_monetary_cost(),
-            payoff_period,
+            benefit_horizon,
+            penalty_multiplier,
         )
-        right_score = _compute_payoff_cost(
-            right.get_transition_time_s(),
-            right.get_transition_cost(),
-            curr_cost_value,
-            right.get_operational_monetary_cost(),
-            payoff_period,
+        right_score = _compute_scalar_score(
+            left.get_transition_time_s(),
+            left.get_transition_cost(),
+            curr_hourly_cost,
+            left.get_operational_monetary_cost(),
+            benefit_horizon,
+            penalty_multiplier,
         )
+
         return left_score < right_score
 
     return is_better_than
 
 
-def _get_or_compute_p99_latency(bp: ComparableBlueprint) -> float:
-    stored = bp.get_memoized_value("p99_latency")
+def _get_or_compute_query_p90_latency(bp: ComparableBlueprint) -> float:
+    stored = bp.get_memoized_value("query_p90_latency")
     if stored is not None:
         return stored
     else:
-        p99_lat = np.quantile(
-            bp.get_predicted_analytical_latencies(), 0.99, method="lower"
+        p90_lat = np.quantile(
+            bp.get_predicted_analytical_latencies(), 0.9, method="lower"
         )
-        bp.set_memoized_value("p99_latency", p99_lat)
-        return p99_lat
+        bp.set_memoized_value("query_p90_latency", p90_lat)
+        return p90_lat
 
 
 def _txn_p90_ceiling(
@@ -90,12 +111,12 @@ def _txn_p90_ceiling(
     return None
 
 
-def _query_ceiling(
+def _query_p90_ceiling(
     left: ComparableBlueprint, right: ComparableBlueprint, query_ceiling_s: float
 ) -> Optional[bool]:
     # Query latency ceilings.
-    left_lat = _get_or_compute_p99_latency(left)
-    right_lat = _get_or_compute_p99_latency(right)
+    left_lat = _get_or_compute_query_p90_latency(left)
+    right_lat = _get_or_compute_query_p90_latency(right)
 
     if left_lat > query_ceiling_s and right_lat > query_ceiling_s:
         # Both are above the latency ceiling.
@@ -109,7 +130,7 @@ def _query_ceiling(
     return None
 
 
-def _transition_under_payoff_period(
+def _transition_under_benefit_horizon(
     left: ComparableBlueprint, right: ComparableBlueprint, payoff_period: timedelta
 ) -> Optional[bool]:
     left_tr_s = left.get_transition_time_s()
@@ -130,19 +151,33 @@ def _transition_under_payoff_period(
     return None
 
 
-def _compute_payoff_cost(
+def _compute_penalty_multiplier(
+    max_query_p90_latency_s: float,
+    max_txn_p90_latency_s: float,
+    curr_query_p90_latency_s: float,
+    curr_txn_p90_latency_s: float,
+    threshold: float,
+) -> float:
+    query_val = curr_query_p90_latency_s / max_query_p90_latency_s
+    txn_val = curr_txn_p90_latency_s / max_txn_p90_latency_s
+    return 1.0 + max(0.0, max(query_val, txn_val) - threshold)
+
+
+def _compute_scalar_score(
     transition_time_s: float,
     transition_cost: float,
     curr_hourly_cost: float,
     next_hourly_cost: float,
-    payoff_period: timedelta,
+    benefit_horizon: timedelta,
+    penalty_multiplier: float,
 ) -> float:
-    leftover_time_s = payoff_period.total_seconds() - transition_time_s
+    leftover_time_s = benefit_horizon.total_seconds() - transition_time_s
+    assert leftover_time_s > 0.0
     leftover_time_hr = leftover_time_s / 60.0 / 60.0
     transition_time_hr = transition_time_s / 60.0 / 60.0
     # Lower is better.
     return (
-        transition_time_hr * curr_hourly_cost
+        transition_time_hr * curr_hourly_cost * penalty_multiplier
         + transition_cost
         + leftover_time_hr * next_hourly_cost
     )
