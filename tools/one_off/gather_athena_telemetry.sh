e8e7d455606af01131878faae79aae4d602e70aa
Geoffrey Yu <geoffxy@mit.edu>
2023-11-23 01:23:50 -0500

    Additional fixes

--- tools/one_off/gather_athena_telemetry.sh
+++ tools/one_off/gather_athena_telemetry.sh
@@ -54,6 +54,6 @@ for epoch in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22; do
         --engines athena \
         --data-s3-bucket geoffxy-research \
         --data-s3-path imdb_specialized_100g/telemetry/telemetry.csv \
-        --times 1 \
+        --load-times 1 \
         --schema-name $schema_name
 done
