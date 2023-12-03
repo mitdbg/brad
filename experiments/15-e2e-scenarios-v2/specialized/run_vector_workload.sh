7f7c280d17bc4c8dfc8c156dc4b57c4615a6657f
Geoffrey Yu <geoffxy@mit.edu>
2023-12-02 23:09:51 -0500

    Increase workload intensity

--- experiments/15-e2e-scenarios-v2/specialized/run_vector_workload.sh
+++ experiments/15-e2e-scenarios-v2/specialized/run_vector_workload.sh
@@ -23,6 +23,7 @@ extract_named_arguments $@
 # 14, 54, 59, 75
 
 query_indices="62,64,65,66,69,72,73,74,91,59"
+heavier_queries="14,54,60,71,75"
 
 # Should be removed eventually and we should rely on the blueprint.
 start_brad $config_file $planner_config_file
@@ -30,7 +31,7 @@ log_workload_point "brad_start_initiated"
 sleep 30
 
 log_workload_point "clients_starting"
-start_repeating_olap_runner 8 5 5 $query_indices "ra_4"
+start_repeating_olap_runner 8 5 5 $query_indices "ra_8"
 rana_pid=$runner_pid
 
 start_sequence_runner 2 8 5 "ra_vector" 4
@@ -47,10 +48,14 @@ function inner_cancel_experiment() {
 trap "inner_cancel_experiment" INT
 trap "inner_cancel_experiment" TERM
 
-sleep $((60 * 60))  # Wait for 1 hour.
+sleep $((5 * 60))  # Wait for 5 mins.
+start_repeating_olap_runner 4 5 5 $heavier_queries "ra_4_heavy"
+heavy_pid=$runner_pid
+
+sleep $((55 * 60))  # Wait for 55 mins.
 log_workload_point "experiment_done"
 
 # Shut down everything now.
 >&2 echo "Experiment done. Shutting down runners..."
-graceful_shutdown $rana_pid $txn_pid $other_pid
+graceful_shutdown $rana_pid $txn_pid $other_pid $heavy_pid
 log_workload_point "shutdown_complete"
