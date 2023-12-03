c2967dcb489f285fd24c29107b18d1aeeb31b78a
Geoffrey Yu <geoffxy@mit.edu>
2023-12-03 16:24:10 -0500

    Additional experiment fixes

--- experiments/15-e2e-scenarios-v2/specialized/run_vector_workload_debug.sh
+++ experiments/15-e2e-scenarios-v2/specialized/run_vector_workload_debug.sh
@@ -23,6 +23,7 @@ extract_named_arguments $@
 # 14, 54, 59, 75
 
 query_indices="62,64,65,66,69,72,73,74,91,59"
+heavier_queries="14,54,60,71,75"
 
 export BRAD_IGNORE_BLUEPRINT=1
 start_brad_debug $config_file $planner_config_file
@@ -30,18 +31,22 @@ log_workload_point "brad_start_initiated"
 sleep 30
 
 log_workload_point "clients_starting"
-start_repeating_olap_runner 4 5 5 $query_indices "ra_4"
+start_repeating_olap_runner 8 5 5 $query_indices "ra_8"
 rana_pid=$runner_pid
 
-start_sequence_runner 2 8 5 "ra_vector" 4
+start_other_repeating_runner 2 8 5 "ra_vector" 8
 other_pid=$runner_pid
 
 start_txn_runner 4  # Implicit: --dataset-type
 txn_pid=$runner_pid
+
+start_repeating_olap_runner 4 5 5 $heavier_queries "ra_4_heavy" 10
+heavy_pid=$runner_pid
+
 log_workload_point "clients_started"
 
 function inner_cancel_experiment() {
-  cancel_experiment $rana_pid $txn_pid $other_pid
+  cancel_experiment $rana_pid $txn_pid $other_pid $heavy_pid
 }
 
 trap "inner_cancel_experiment" INT
@@ -52,5 +57,5 @@ log_workload_point "experiment_done"
 
 # Shut down everything now.
 >&2 echo "Experiment done. Shutting down runners..."
-graceful_shutdown $rana_pid $txn_pid $other_pid
+graceful_shutdown $rana_pid $txn_pid $other_pid $heavy_pid
 log_workload_point "shutdown_complete"
