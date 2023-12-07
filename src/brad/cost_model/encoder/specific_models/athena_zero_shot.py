fd28c10d24c4e5ceb9c52b55a04ee0076f0429fd
wuziniu <wuziniu2015@gmail.com>
2023-12-07 17:01:33 -0500

    commit native system cost model baseline

--- src/brad/cost_model/encoder/specific_models/athena_zero_shot.py
+++ src/brad/cost_model/encoder/specific_models/athena_zero_shot.py
@@ -1,5 +1,9 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 from brad.cost_model.dataset.query_featurization import athena_query_featurization
 from brad.cost_model.encoder.zero_shot_model import ZeroShotModel
+from workloads.cross_db_benchmark.benchmark_tools.aurora.utils import _GetJoinConds
 
 
 class AthenaZeroShotModel(ZeroShotModel):
@@ -47,5 +51,92 @@ class AthenaZeroShotModel(ZeroShotModel):
             encoders=encoders,
             prepasses=prepasses,
             add_tree_model_types=tree_model_types,
-            **zero_shot_kwargs
+            **zero_shot_kwargs,
         )
+
+
+class AthenaNativeCostModel:
+    def __init__(self, slope=1.0, intercept=0, model=None):
+        self.slope = slope
+        self.intercept = intercept
+        self.model = model
+
+    def parse_raw(
+        self,
+        raw,
+        min_query_runtime=100,
+        test_query=None,
+        min_num_tables=None,
+        max_num_tables=None,
+    ):
+        feature = []
+        label = []
+        test_idx = []
+
+        for q in raw["query_list"]:
+            if (
+                q["status"] != "SUCCEEDED"
+                or q["runtime_stats"] is None
+                or len(q["runtime_stats"]) == 0
+            ):
+                continue
+            runtime = q["runtime_stats"]["Timeline"]["TotalExecutionTimeInMillis"]
+            if runtime > min_query_runtime:
+                label.append(runtime)
+            else:
+                continue
+
+            est_cost = q["runtime_stats"]["Rows"]["InputBytes"] / 1024 / 1024
+            feature.append(est_cost)
+
+            if test_query is not None:
+                sql = q["exec_info"]["Query"] + ";"
+                if sql in test_query:
+                    test_idx.append(len(label) - 1)
+            elif min_num_tables is not None or max_num_tables is not None:
+                if min_num_tables is None:
+                    min_num_tables = 0
+                if max_num_tables is None:
+                    max_num_tables = 1000
+                sql = q["exec_info"]["Query"] + ";"
+                num_tables = len(_GetJoinConds(sql)) + 1
+                if min_num_tables <= num_tables <= max_num_tables:
+                    test_idx.append(len(label) - 1)
+
+        train_feature = [feature[i] for i in range(len(feature)) if i not in test_idx]
+        test_feature = [feature[i] for i in range(len(feature)) if i in test_idx]
+        train_label = [label[i] for i in range(len(label)) if i not in test_idx]
+        test_label = [label[i] for i in range(len(label)) if i in test_idx]
+        return train_feature, train_label, test_feature, test_label
+
+    def evaluate(self, feature, label):
+        pred = self.model.predict(feature)
+        pred = np.abs(pred)
+        qerror = np.maximum(label / pred, pred / label)
+        for i in [50, 95, 99]:
+            print(f"{i} percentile is {np.percentile(qerror, i)}")
+        return pred / 1000
+
+    def train_and_test(
+        self,
+        raw,
+        min_query_runtime=100,
+        test_query=None,
+        min_num_tables=None,
+        max_num_tables=None,
+    ):
+        train_feature, train_label, test_feature, test_label = self.parse_raw(
+            raw, min_query_runtime, test_query, min_num_tables, max_num_tables
+        )
+        train_feature = np.asarray(train_feature).reshape(-1, 1)
+        train_label = np.asarray(train_label)
+        model = LinearRegression()
+        model.fit(train_feature, train_label)
+        self.model = model
+        self.slope = model.coef_
+        self.intercept = model.intercept_
+
+        test_feature = np.asarray(test_feature).reshape(-1, 1)
+        test_label = np.asarray(test_label)
+        pred = self.evaluate(test_feature, test_label)
+        return pred
--- src/brad/cost_model/encoder/specific_models/aurora_zero_shot.py
+++ src/brad/cost_model/encoder/specific_models/aurora_zero_shot.py
@@ -1,6 +1,10 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
 from brad.cost_model.dataset.plan_featurization import aurora_plan_featurizations
 from brad.cost_model.dataset.query_featurization import aurora_query_featurization
 from brad.cost_model.encoder.zero_shot_model import ZeroShotModel
+from workloads.cross_db_benchmark.benchmark_tools.aurora.utils import _GetJoinConds
 
 
 class AuroraZeroShotModelPlan(ZeroShotModel):
@@ -44,7 +48,7 @@ class AuroraZeroShotModelPlan(ZeroShotModel):
             encoders=encoders,
             prepasses=prepasses,
             add_tree_model_types=tree_model_types,
-            **zero_shot_kwargs
+            **zero_shot_kwargs,
         )
 
 
@@ -93,5 +97,100 @@ class AuroraZeroShotModel(ZeroShotModel):
             encoders=encoders,
             prepasses=prepasses,
             add_tree_model_types=tree_model_types,
-            **zero_shot_kwargs
+            **zero_shot_kwargs,
         )
+
+
+class AuroraNativeCostModel:
+    def __init__(self, slope=1.0, intercept=0, model=None):
+        self.slope = slope
+        self.intercept = intercept
+        self.model = model
+
+    def parse_raw(
+        self,
+        raw,
+        min_query_runtime=100,
+        test_query=None,
+        min_num_tables=None,
+        max_num_tables=None,
+    ):
+        feature = []
+        label = []
+        test_idx = []
+
+        for q in raw["query_list"]:
+            if (
+                q["timeout"]
+                or q["verbose_plan"] is None
+                or len(q["verbose_plan"]) == 0
+                or q["analyze_plans"] is None
+                or len(q["analyze_plans"]) == 0
+            ):
+                continue
+            runtimes = []
+            for plan in q["analyze_plans"]:
+                runtime = float(
+                    plan[-1][0].split("Execution Time:")[-1].split("ms")[0].strip()
+                )
+                runtimes.append(runtime)
+            if np.mean(runtimes) > min_query_runtime:
+                label.append(np.mean(runtimes))
+            else:
+                continue
+
+            est_cost = float(
+                q["verbose_plan"][0][0].split("..")[-1].split(" ")[0].strip()
+            )
+            feature.append(est_cost)
+
+            if test_query is not None:
+                if q["sql"] in test_query:
+                    test_idx.append(len(label) - 1)
+            elif min_num_tables is not None or max_num_tables is not None:
+                if min_num_tables is None:
+                    min_num_tables = 0
+                if max_num_tables is None:
+                    max_num_tables = 1000
+                sql = q["sql"]
+                num_tables = len(_GetJoinConds(sql)) + 1
+                if min_num_tables <= num_tables <= max_num_tables:
+                    test_idx.append(len(label) - 1)
+
+        train_feature = [feature[i] for i in range(len(feature)) if i not in test_idx]
+        test_feature = [feature[i] for i in range(len(feature)) if i in test_idx]
+        train_label = [label[i] for i in range(len(label)) if i not in test_idx]
+        test_label = [label[i] for i in range(len(label)) if i in test_idx]
+        return train_feature, train_label, test_feature, test_label
+
+    def evaluate(self, feature, label):
+        pred = self.model.predict(feature)
+        pred = np.abs(pred)
+        qerror = np.maximum(label / pred, pred / label)
+        for i in [50, 95, 99]:
+            print(f"{i} percentile is {np.percentile(qerror, i)}")
+        return pred / 1000
+
+    def train_and_test(
+        self,
+        raw,
+        min_query_runtime=100,
+        test_query=None,
+        min_num_tables=None,
+        max_num_tables=None,
+    ):
+        train_feature, train_label, test_feature, test_label = self.parse_raw(
+            raw, min_query_runtime, test_query, min_num_tables, max_num_tables
+        )
+        train_feature = np.asarray(train_feature).reshape(-1, 1)
+        train_label = np.asarray(train_label)
+        model = LinearRegression()
+        model.fit(train_feature, train_label)
+        self.model = model
+        self.slope = model.coef_
+        self.intercept = model.intercept_
+
+        test_feature = np.asarray(test_feature).reshape(-1, 1)
+        test_label = np.asarray(test_label)
+        pred = self.evaluate(test_feature, test_label)
+        return pred
