a58f0ff0dddc96a25804d498066fd3ba95349890
Geoffrey Yu <geoffxy@mit.edu>
2023-12-10 13:39:49 -0500

    Separate out for better use

--- tools/plots/e2e.py
+++ tools/plots/e2e.py
@@ -1,39 +1,7 @@
 import pathlib
-from typing import List, Optional, Any, Dict, Tuple
+from typing import Optional, Tuple
 
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as font_manager
-
-
-def set_global_plt_configs(
-    use_libertine_font: bool = True, font_dirs: Optional[List[str]] = None
-):
-    if font_dirs is None:
-        inner_font_dirs = [pathlib.Path.home() / ".local/share/fonts"]
-    else:
-        inner_font_dirs = [pathlib.Path(font_dir) for font_dir in font_dirs]
-
-    configs: Dict[str, Any] = {
-        "font.size": 14,
-        "pdf.fonttype": 42,  # Ensures type 1 fonts, apparently.
-    }
-
-    # Matches the font used in the VLDB template (circa 2023).
-    if use_libertine_font:
-        font_files = font_manager.findSystemFonts(fontpaths=inner_font_dirs)
-        font_manager_instance = font_manager.FontManager()
-        for font in font_files:
-            font_manager_instance.addfont(font)
-        configs["font.family"] = "Linux Libertine"
-
-    plt.rcParams.update(configs)
-
-
-COLORS = [
-    "#00188d",  # 0, 24, 141 (dark blue)
-    "#59b4d9",  # 89, 180, 217 (light blue)
-]
 
 
 def load_txn_data(
@@ -149,7 +117,3 @@ class RecordedRun:
         self.txn_stats = txn_stats
         self.events = events
         self.txn_lats = txn_lats
-
-
-if __name__ == "__main__":
-    set_global_plt_configs(use_libertine_font=True)
