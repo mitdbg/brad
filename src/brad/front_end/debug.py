5494578eac3f174f8a0208817e5c9730d37be319
Geoffrey Yu <geoffxy@mit.edu>
2024-01-25 14:37:23 -0500

    Additional fixes (#443)
    * Collect more connection data
    
    * Switch back to ODBC for now
    
    * Enumeration adjustments

--- src/brad/front_end/debug.py
+++ src/brad/front_end/debug.py
@@ -14,11 +14,19 @@ class ReestablishConnectionsReport:
         self.connection_failures[Engine.Athena] = 0
         self.connection_failures[Engine.Redshift] = 0
 
+        self.still_connected: Dict[Engine, int] = {}
+        self.still_connected[Engine.Aurora] = 0
+        self.still_connected[Engine.Athena] = 0
+        self.still_connected[Engine.Redshift] = 0
+
     def bump(self, engine: Engine, succeeded: bool) -> None:
         if succeeded:
             self.connection_successes[engine] += 1
         else:
             self.connection_failures[engine] += 1
+
+    def bump_still_connected(self, engine: Engine) -> None:
+        self.still_connected[engine] += 1
 
     def merge(
         self, other: "ReestablishConnectionsReport"
@@ -26,6 +34,7 @@ class ReestablishConnectionsReport:
         for engine in [Engine.Aurora, Engine.Athena, Engine.Redshift]:
             self.connection_successes[engine] += other.connection_successes[engine]
             self.connection_failures[engine] += other.connection_failures[engine]
+            self.still_connected[engine] += other.still_connected[engine]
         return self
 
     def all_succeeded(self) -> bool:
@@ -39,5 +48,6 @@ class ReestablishConnectionsReport:
         for engine in [Engine.Aurora, Engine.Athena, Engine.Redshift]:
             succeeded = self.connection_successes[engine]
             total = self.connection_successes[engine] + self.connection_failures[engine]
-            parts.append(f"{engine.value}={succeeded}/{total}")
+            still = self.still_connected[engine]
+            parts.append(f"{engine.value}={succeeded}/{total}/{still}")
         return "".join(["ReestablishConnectionsReport(", ", ".join(parts), ")"])
