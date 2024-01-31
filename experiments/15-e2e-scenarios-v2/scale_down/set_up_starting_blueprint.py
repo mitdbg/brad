e0e94e9855aed1a9ec480199cbd75a2b3c96775b
Geoffrey Yu <geoffxy@mit.edu>
2024-01-30 20:16:56 -0500

    Adjust initial blueprint

--- experiments/15-e2e-scenarios-v2/scale_down/set_up_starting_blueprint.py
+++ experiments/15-e2e-scenarios-v2/scale_down/set_up_starting_blueprint.py
@@ -124,7 +124,7 @@ def main():
 
     # Ensure the provisioning is as expected.
     enum_blueprint.set_aurora_provisioning(Provisioning("db.r6g.xlarge", 1))
-    enum_blueprint.set_redshift_provisioning(Provisioning("dc2.large", 1))
+    enum_blueprint.set_redshift_provisioning(Provisioning("dc2.large", 2))
 
     # 6. Adjust the placement.
     new_placement = {}
