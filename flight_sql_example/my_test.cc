0fbbbedbfdd8bc3259c4ad04483b3e74c2103d25
Sophie Zhang <sopzha@gmail.com>
2024-02-15 19:22:28 -0500

    Using FetchContent to access arrow repo in my_test example

---
+++ flight_sql_example/my_test.cc
@@ -0,0 +1,16 @@
+#include <arrow/flight/client.h>
+
+namespace arrow {
+namespace flight {
+	TimeoutDuration td;
+}
+}
+
+arrow::Status Main() {
+	// ARROW_ASSIGN_OR_RAISE(auto location, arrow::flight::Location::ForGrpcTcp("localhost", 31337));
+}
+
+int main() {
+	return 0;
+}
+
