60204f0f541ff4ce36076406028d70fd273afd14
Geoffrey Yu <geoffxy@mit.edu>
2024-03-05 16:28:30 -0500

    Check in structure

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div class="header">
+        <div class="header-inner">BRAD</div>
+      </div>
+      <div class="body-container">
+        <div class="left-column">
+          <h2>Data Infrastructure</h2>
+          <div class="panel">
+            <h2>Virtual Database Engines</h2>
+          </div>
+          <div class="panel">
+            <h2>Current Blueprint (Physical Infrastructure)</h2>
+          </div>
+        </div>
+        <div class="right-column">
+          <h2>Performance Monitoring</h2>
+          <div class="panel"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
