4795a69551c9a28c6535c5a8d2d54baa4c1901b5
Geoffrey Yu <geoffxy@mit.edu>
2024-03-05 16:58:48 -0500

    Fix layout styling

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -7,18 +7,22 @@ function App() {
         <div class="header-inner">BRAD</div>
       </div>
       <div class="body-container">
-        <div class="left-column">
+        <div class="column">
           <h2>Data Infrastructure</h2>
-          <div class="panel">
-            <h2>Virtual Database Engines</h2>
-          </div>
-          <div class="panel">
-            <h2>Current Blueprint (Physical Infrastructure)</h2>
+          <div class="column-inner">
+            <div class="panel">
+              <h2>Virtual Database Engines</h2>
+            </div>
+            <div class="panel">
+              <h2>Current Blueprint (Physical Infrastructure)</h2>
+            </div>
           </div>
         </div>
-        <div class="right-column">
+        <div class="column">
           <h2>Performance Monitoring</h2>
-          <div class="panel"></div>
+          <div class="column-inner">
+            <div class="panel"></div>
+          </div>
         </div>
       </div>
     </>
