3ed9ec3b29c39fbd9746850b2f624fc358963e8c
Geoffrey Yu <geoffxy@mit.edu>
2024-03-07 17:21:15 -0500

    Add VDBE view

--- ui/src/components/VirtualInfraView.jsx
+++ ui/src/components/VirtualInfraView.jsx
@@ -1,7 +1,12 @@
 import Panel from "./Panel";
+import DbCylinder from "./DbCylinder";
 
 function VirtualInfraView() {
-  return <Panel heading="Virtual Databases"></Panel>;
+  return (
+    <Panel heading="Virtual Databases">
+      <DbCylinder color="blue">VDBE 1</DbCylinder>
+    </Panel>
+  );
 }
 
 export default VirtualInfraView;
