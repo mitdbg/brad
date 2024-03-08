18e4337b9be39c22df1a430f92d56e93df03d404
Geoffrey Yu <geoffxy@mit.edu>
2024-03-08 13:36:20 -0500

    Render system metrics and blueprint (#473)
    * Add performance data chart
    
    * Add endpoint for live metrics
    
    * Add endpoint to return blueprint state
    
    * Fetch data from the daemon, parse and feed it to the views
    
    * Fix metrics state updating
    
    * Link system state too
    
    * Ensure charts update live
    
    * Pass through other metrics
    
    * Styling fixes, display actual blueprint
    
    * Fix types

--- ui/src/components/PerfView.jsx
+++ ui/src/components/PerfView.jsx
@@ -1,7 +1,54 @@
 import Panel from "./Panel";
+import LatencyPlot from "./LatencyPlot";
+import "./styles/PerfView.css";
 
-function PerfView() {
-  return <Panel></Panel>;
+function extractMetrics(data, metricName, multiplier) {
+  if (multiplier == null) {
+    multiplier = 1.0;
+  }
+  if (!data.hasOwnProperty(metricName)) {
+    return {
+      x: [],
+      y: [],
+    };
+  } else {
+    const metrics = data[metricName];
+    return {
+      x: metrics.timestamps.map((_, idx) => idx),
+      y: metrics.values.map((val) => val * multiplier),
+    };
+  }
+}
+
+function PerfView({ metricsData }) {
+  const queryLatMetrics = extractMetrics(metricsData, "query_latency_s_p90");
+  const txnLatMetrics = extractMetrics(metricsData, "txn_latency_s_p90");
+  return (
+    <Panel>
+      <div class="perf-view-wrap">
+        <div>
+          <h2>Query Latency</h2>
+          <LatencyPlot
+            seriesName="Query Latency"
+            labels={queryLatMetrics.x}
+            values={queryLatMetrics.y}
+            xLabel="Elapsed Time (minutes)"
+            yLabel="p90 Latency (s)"
+          />
+        </div>
+        <div style={{"marginTop": "30px"}}>
+          <h2>Transaction Latency</h2>
+          <LatencyPlot
+            seriesName="Transaction Latency"
+            labels={txnLatMetrics.x}
+            values={txnLatMetrics.y}
+            xLabel="Elapsed Time (minutes)"
+            yLabel="p90 Latency (ms)"
+          />
+        </div>
+      </div>
+    </Panel>
+  );
 }
 
 export default PerfView;
