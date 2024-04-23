277f641644d09908416fec19137fc215f27c3ae8
Geoffrey Yu <geoffxy@mit.edu>
2024-04-23 17:54:42 -0400

    Add schema deduction to Aurora

--- src/brad/connection/odbc_cursor.py
+++ src/brad/connection/odbc_cursor.py
@@ -1,7 +1,10 @@
 import asyncio
+import datetime
+import decimal
 from typing import Any, Optional, List, Iterable
 
 from .cursor import Cursor, Row
+from .schema import Schema, Field, DataType
 
 
 class OdbcCursor(Cursor):
@@ -19,7 +22,9 @@ class OdbcCursor(Cursor):
 
     async def fetchall(self) -> List[Row]:
         loop = asyncio.get_running_loop()
-        return await loop.run_in_executor(None, self._impl.fetchall)
+        res = await loop.run_in_executor(None, self._impl.fetchall)
+        print("ODBC", self._impl.description)
+        return res
 
     async def commit(self) -> None:
         loop = asyncio.get_running_loop()
@@ -39,7 +44,30 @@ class OdbcCursor(Cursor):
         return self._impl.fetchone()
 
     def fetchall_sync(self) -> List[Row]:
-        return self._impl.fetchall()
+        res = self._impl.fetchall()
+        return res
+
+    def result_schema(self) -> Schema:
+        fields = []
+        for column_metadata in self._impl.description:
+            column_name = column_metadata[0]
+            odbc_type = column_metadata[1]
+            if odbc_type is int:
+                brad_type = DataType.Integer
+            elif odbc_type is str:
+                brad_type = DataType.String
+            elif odbc_type is float:
+                brad_type = DataType.Float
+            elif odbc_type is bool:
+                brad_type = DataType.Integer
+            elif odbc_type is decimal.Decimal:
+                brad_type = DataType.Decimal
+            elif odbc_type is datetime.datetime:
+                brad_type = DataType.Timestamp
+            else:
+                brad_type = DataType.Unknown
+            fields.append(Field(name=column_name, data_type=brad_type))
+        return Schema(fields)
 
     def commit_sync(self) -> None:
         self._impl.commit()
