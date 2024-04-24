84ccdd33613132c90ca758d48906796cd833a4de
Geoffrey Yu <geoffxy@mit.edu>
2024-04-24 15:34:24 -0400

    Implement initial TPC-C runner experiment defs

---
+++ experiments/17-chbenchmark/common.sh
@@ -0,0 +1,69 @@
+function extract_named_arguments() {
+  # Evaluates any environment variables in this script's arguments. This script
+  # should only be run on trusted input.
+  orig_args=($@)
+  for val in "${orig_args[@]}"; do
+    phys_arg=$(eval "echo $val")
+
+    if [[ $phys_arg =~ --ra-clients=.+ ]]; then
+      ra_clients=${phys_arg:13}
+    fi
+
+    if [[ $phys_arg =~ --t-clients=.+ ]]; then
+      t_clients=${phys_arg:12}
+    fi
+
+    if [[ $phys_arg =~ --ra-query-indexes=.+ ]]; then
+      ra_query_indexes=${phys_arg:19}
+    fi
+
+    if [[ $phys_arg =~ --ra-query-bank-file=.+ ]]; then
+      ra_query_bank_file=${phys_arg:21}
+    fi
+
+    if [[ $phys_arg =~ --ra-gap-s=.+ ]]; then
+      ra_gap_s=${phys_arg:11}
+    fi
+
+    if [[ $phys_arg =~ --ra-gap-std-s=.+ ]]; then
+      ra_gap_std_s=${phys_arg:15}
+    fi
+
+    if [[ $phys_arg =~ --num-front-ends=.+ ]]; then
+      num_front_ends=${phys_arg:17}
+    fi
+
+    if [[ $phys_arg =~ --run-for-s=.+ ]]; then
+      run_for_s=${phys_arg:12}
+    fi
+
+    if [[ $phys_arg =~ --physical-config-file=.+ ]]; then
+      physical_config_file=${phys_arg:23}
+    fi
+
+    if [[ $phys_arg =~ --system-config-file=.+ ]]; then
+      system_config_file=${phys_arg:21}
+    fi
+
+    if [[ $phys_arg =~ --schema-name=.+ ]]; then
+      schema_name=${phys_arg:14}
+    fi
+
+    if [[ $phys_arg =~ --query-sequence-file=.+ ]]; then
+      query_sequence_file=${phys_arg:22}
+    fi
+
+    if [[ $phys_arg =~ --txn-scale-factor=.+ ]]; then
+      txn_scale_factor=${phys_arg:19}
+    fi
+
+    if [[ $phys_arg =~ --txn-warehouses=.+ ]]; then
+      txn_warehouses=${phys_arg:17}
+    fi
+
+    if [[ $phys_arg =~ --txn-config-file=.+ ]]; then
+      txn_config_file=${phys_arg:18}
+    fi
+  done
+}
+
