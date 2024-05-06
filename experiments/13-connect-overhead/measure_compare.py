c7ae83fd12675afd0f43a2493c507adcc5499609
Sophie Zhang <sopzha@gmail.com>
2024-05-05 23:49:36 -0400

    Compare query execution latencies for different connections

---
+++ experiments/13-connect-overhead/measure_compare.py
@@ -0,0 +1,44 @@
+import argparse
+import time
+from brad.grpc_client import BradGrpcClient
+from brad.flight_sql_client import BradFlightSqlClient
+from prettytable import PrettyTable
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--repetitions", type=int, default=1000)
+    parser.add_argument("--query", type=str, default="SELECT 1")
+    args = parser.parse_args()
+
+    table = PrettyTable()
+    table.field_names = ["Connection Type", "Reps", "Total Time (s)", "Average Latency (s)"]
+
+    # TODO: Remove hardcoding
+    with BradGrpcClient(host="localhost", port=6583) as client:
+        start = time.time()
+        for _ in range(args.repetitions):
+            client.run_query_json(args.query)
+        end = time.time()
+
+    total = end - start
+    avg_lat = total / args.repetitions
+
+    table.add_row(["gRPC", args.repetitions, total, avg_lat])
+
+    with BradFlightSqlClient(host="localhost", port=31337) as client:
+        start = time.time()
+        for _ in range(args.repetitions):
+            client.run_query(args.query)
+        end = time.time()
+
+    total = end - start
+    avg_lat = total / args.repetitions
+
+    table.add_row(["Flight SQL ODBC", args.repetitions, total, avg_lat])
+
+    print(table)
+
+
+if __name__ == "__main__":
+    main()
