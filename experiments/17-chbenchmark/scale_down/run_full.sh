ddd44d24cef5c402309f757fdb96176c71eca8aa
Geoffrey Yu <geoffxy@mit.edu>
2024-05-10 14:57:14 -0400

    CH-BenCHmark: Adjust starting config again

--- experiments/17-chbenchmark/scale_down/run_full.sh
+++ experiments/17-chbenchmark/scale_down/run_full.sh
@@ -10,12 +10,11 @@ abs_txn_config_file=$(realpath $txn_config_file)
 abs_system_config_file=$(realpath $system_config_file)
 abs_physical_config_file=$(realpath $physical_config_file)
 
-export BRAD_IGNORE_BLUEPRINT=1
 start_brad $abs_system_config_file $abs_physical_config_file
 
 sleep 30
 
-run_tpcc "t_1"
+run_tpcc "t_4"
 start_repeating_olap_runner 1 10 5 $ra_query_indexes "ch_1" $t_clients
 ra_pid=$runner_pid
 
--- experiments/17-chbenchmark/scale_down/set_up_starting_blueprint.sh
+++ experiments/17-chbenchmark/scale_down/set_up_starting_blueprint.sh
@@ -12,8 +12,8 @@ source ../common.sh
 python3 ../../../workloads/IMDB_extended/set_up_starting_blueprint.py \
   --schema-name chbenchmark \
   --query-bank-file ../../../workloads/chbenchmark/queries.sql \
-  --redshift-queries "0,1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21" \
-  --athena-queries "4" \
+  --redshift-queries "0,1,2,3,5,6,7,8,10,11,12,13,14,15,16,18,19,20,21" \
+  --athena-queries "4,9,17" \
   --redshift-provisioning "dc2.large:16" \
   --aurora-provisioning "db.r6g.2xlarge:1" \
   --system-config-file ch_scale_down_config.yml \
