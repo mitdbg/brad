4198d19bc38ac6d109a84b0d954700b4532492b6
Sophie Zhang <sopzha@gmail.com>
2024-05-11 23:46:53 -0400

    Build pandas dataframe from latency measurement data and save to csv

--- experiments/13-connect-overhead/measure_compare.py
+++ experiments/13-connect-overhead/measure_compare.py
@@ -3,54 +3,78 @@ import time
 from brad.grpc_client import BradGrpcClient
 from brad.flight_sql_client_odbc import BradFlightSqlClientOdbc
 from brad.sqlite_client import BradSqliteClient
-from prettytable import PrettyTable
+import pandas
+import numpy as np
+from collections import defaultdict
+
+def run_client(client, trials, repetitions, query) -> tuple[float, float]:
+    total_lat = 0
+    lats = []
+    for _ in range(trials):
+        start = time.time()
+        for _ in range(repetitions):
+            if isinstance(client, BradGrpcClient):
+                client.run_query_json(query)
+            else:
+                client.run_query(query)
+        end = time.time()
+
+        total = end - start
+        lat = total / repetitions
+
+        lats.append(lat)
+        total_lat += lat
+    
+    return total_lat / trials, np.std(lats)
 
 
-def main():
+def build_dataframe() -> pandas.DataFrame:
     parser = argparse.ArgumentParser()
     parser.add_argument("--repetitions", type=int, default=10000)
+    parser.add_argument("--trials", type=int, default=5)
     parser.add_argument("--query", type=str, default="SELECT 1")
     args = parser.parse_args()
 
-    table = PrettyTable()
-    table.field_names = ["Connection Type", "Reps", "Total Time (s)", "Average Latency (s)"]
+    queries = [
+        "SELECT 1;",
+        "SELECT * FROM person_info;",
+        "SELECT * FROM person_info WHERE id = 123456;"
+    ]
 
-    # TODO: Remove hardcoding
+    data = defaultdict(list)
+
+    data["Connection Type"].append("gRPC")
     with BradGrpcClient(host="localhost", port=6583) as client:
-        start = time.time()
-        for _ in range(args.repetitions):
-            client.run_query_json(args.query)
-        end = time.time()
-
-    total = end - start
-    avg_lat = total / args.repetitions
-
-    table.add_row(["gRPC", args.repetitions, total, avg_lat])
-
+        for query in queries:
+            lat_avg, lat_std_dev = run_client(client, args.trials, args.repetitions, query)
+            data[f"Latency (Avg, Std Dev) for `{query}`"].append((lat_avg, lat_std_dev))
+        
+    data["Connection Type"].append("Flight SQL ODBC")
     with BradFlightSqlClientOdbc(host="localhost", port=31337) as client:
-        start = time.time()
-        for _ in range(args.repetitions):
-            client.run_query(args.query)
-        end = time.time()
+        for query in queries:
+            lat_avg, lat_std_dev = run_client(client, args.trials, args.repetitions, query)
+            data[f"Latency (Avg, Std Dev) for `{query}`"].append((lat_avg, lat_std_dev))
 
-    total = end - start
-    avg_lat = total / args.repetitions
-
-    table.add_row(["Flight SQL ODBC", args.repetitions, total, avg_lat])
-
+    data["Connection Type"].append("SQLite")
     with BradSqliteClient(database="/tmp/sophiez_brad_stub_db.sqlite") as client:
-        start = time.time()
-        for _ in range(args.repetitions):
-            client.run_query(args.query)
-        end = time.time()
+        for query in queries:
+            lat_avg, lat_std_dev = run_client(client, args.trials, args.repetitions, query)
+            data[f"Latency (Avg, Std Dev) for `{query}`"].append((lat_avg, lat_std_dev))
 
-    total = end - start
-    avg_lat = total / args.repetitions
+    return pandas.DataFrame.from_dict(data)
 
-    table.add_row(["SQLite", args.repetitions, total, avg_lat])
+def print_to_csv(dataframe: pandas.DataFrame, filename: str) -> None:
+    dataframe.to_csv(filename, index=False)
 
-    print(table)
+def plot_from_csv(filename: str) -> None:
+    dataframe = pandas.read_csv(filename)
+    print(dataframe)
 
+def main():
+    dataframe = build_dataframe()
+    csv_filename = "measurement_comparisons.csv"
+    print_to_csv(dataframe, csv_filename)
+    plot_from_csv(csv_filename)
 
 if __name__ == "__main__":
     main()
