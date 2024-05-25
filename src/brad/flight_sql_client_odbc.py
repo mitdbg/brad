a354eff46b83a4ceeb35e8345ce4c57fbfb5c727
Sophie Zhang <sopzha@gmail.com>
2024-05-25 11:27:34 -0400

    Fix mypy type check

--- src/brad/flight_sql_client_odbc.py
+++ src/brad/flight_sql_client_odbc.py
@@ -1,5 +1,5 @@
 import pyodbc
-from typing import Generator, Tuple, List, Any
+from typing import Generator, Optional, Self, Tuple, List, Any
 
 
 class BradFlightSqlClientOdbc:
@@ -8,8 +8,8 @@ class BradFlightSqlClientOdbc:
 
     Usage:
     ```
-    with BradFlightSqlClient(host, port) as client:
-        for row in client.run_query(session_id, "SELECT 1"):
+    with BradFlightSqlClientOdbc(host, port) as client:
+        for row in client.run_query("SELECT 1"):
             print(row)
     ```
     """
@@ -19,10 +19,10 @@ class BradFlightSqlClientOdbc:
     def __init__(self, host="localhost", port=31337) -> None:
         self._host = host
         self._port = port
-        self._connection = None
-        self._cursor = None
+        self._connection: Optional[pyodbc.Connection] = None
+        self._cursor: Optional[pyodbc.Cursor] = None
 
-    def __enter__(self) -> None:
+    def __enter__(self) -> Self:
         self.connect()
         return self
 
@@ -39,12 +39,16 @@ class BradFlightSqlClientOdbc:
         self._cursor = self._connection.cursor()
 
     def close(self) -> None:
+        assert self._cursor
+        assert self._connection
         self._cursor.close()
         self._connection.close()
 
     def run_query_generator(self, query: str) -> Generator[Tuple[Any, ...], None, None]:
+        assert self._cursor
         for row in self._cursor.execute(query):
             yield row
 
     def run_query(self, query: str) -> RowList:
+        assert self._cursor
         return self._cursor.execute(query)
--- src/brad/sqlite_client.py
+++ src/brad/sqlite_client.py
@@ -1,5 +1,5 @@
 import sqlite3
-from typing import Generator, Tuple, List, Any
+from typing import Generator, Optional, Self, Tuple, Any
 
 
 class BradSqliteClient:
@@ -8,20 +8,18 @@ class BradSqliteClient:
 
     Usage:
     ```
-    with BradSqlClient(host, port) as client:
+    with BradSqliteClient(database) as client:
         for row in client.run_query(session_id, "SELECT 1"):
             print(row)
     ```
     """
 
-    RowList = List[Tuple[Any, ...]]
-
     def __init__(self, database: str) -> None:
         self._database = database
-        self._connection = None
-        self._cursor = None
+        self._connection: Optional[sqlite3.Connection] = None
+        self._cursor: Optional[sqlite3.Cursor] = None
 
-    def __enter__(self) -> None:
+    def __enter__(self) -> Self:
         self.connect()
         return self
 
@@ -33,6 +31,8 @@ class BradSqliteClient:
         self._cursor = self._connection.cursor()
 
     def close(self) -> None:
+        assert self._cursor
+        assert self._connection
         self._cursor.close()
         self._connection.close()
 
@@ -41,6 +41,6 @@ class BradSqliteClient:
         for row in self._cursor.execute(query):
             yield row
 
-    def run_query(self, query: str) -> RowList:
+    def run_query(self, query: str) -> sqlite3.Cursor:
         assert self._cursor
         return self._cursor.execute(query)
