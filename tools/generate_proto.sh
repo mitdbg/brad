b8200fb83aa2df221e84933538b55a979461b772
Geoffrey Yu <geoffxy@mit.edu>
2024-09-12 14:00:53 -0400

    Protobuf definition fixes, update generate script

--- tools/generate_proto.sh
+++ tools/generate_proto.sh
@@ -4,6 +4,20 @@ SCRIPT_PATH=$(cd $(dirname $0) && pwd -P)
 cd $SCRIPT_PATH
 source shared.sh
 
+# NOTE: You need to manually add `# type: ignore` to the .pyi stubs, since the
+# generated code causes an error in mypy's type check.
+python3 -m grpc_tools.protoc \
+  -I../proto \
+  --python_out=../src/brad/proto_gen \
+  --pyi_out=../src/brad/proto_gen \
+  ../proto/interface.proto \
+  ../proto/interface/blueprint.proto \
+  ../proto/interface/schema.proto \
+  ../proto/interface/vdbe.proto
+
+# Fix the import path.
+sed -i -e "s/from interface import/from brad.proto_gen.interface import/g" ../src/brad/proto_gen/interface_pb2.py
+
 python3 -m grpc_tools.protoc \
   -I../proto \
   --python_out=../src/brad/proto_gen \
