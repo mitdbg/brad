39ab1ec50655d135e10fce0893dac822dd109039
Geoffrey Yu <geoffxy@mit.edu>
2025-01-22 16:12:05 -0500

    Add VDBE model support for demo purposes

---
+++ src/brad/vdbe/models.py
@@ -0,0 +1,35 @@
+import enum
+from typing import List
+from pydantic import BaseModel
+
+# This is a simple implementation of a Virtual Database Engine (VDBE) metadata
+# model meant for demonstration purposes only.
+
+
+class VirtualTable(BaseModel):
+    name: str
+    writable: bool
+
+
+class SchemaTable(BaseModel):
+    name: str
+
+
+class QueryInterface(enum.Enum):
+    Common = "common"
+    PostgreSQL = "postgresql"
+    Athena = "athena"
+
+
+class VirtualEngine(BaseModel):
+    name: str
+    max_staleness_ms: int
+    p90_latency_slo_ms: int
+    interface: QueryInterface
+    tables: List[VirtualTable]
+
+
+class VirtualInfrastructure(BaseModel):
+    schema_name: str
+    engines: List[VirtualEngine]
+    tables: List[SchemaTable]
